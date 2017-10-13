adad1cdfc910efaaa0d56cdeff8ddd28977db3c0
Matthew N. White <mnwhite@gmail.com>
2017-10-13 08:51:30 -0400

    Longer simulations
    Committing to send these changes to Edmund.  I think this just lengthens the simulation time.

--- cAndCwithStickyE/StickyEparams.py
+++ cAndCwithStickyE/StickyEparams.py
@@ -18,7 +18,7 @@ from HARKutilities import approxUniform
 
 # Choose basic simulation parameters
 UpdatePrb = 0.25       # Probability that each agent observes the aggregate productivity state each period (in sticky version)
-periods_to_sim = 5010  # Total number of periods to simulate; this might be increased by DSGEmarkov model
+periods_to_sim = 21010 # Total number of periods to simulate; this might be increased by DSGEmarkov model
 ignore_periods = 1000  # Number of simulated periods to ignore (in order to ensure we are near steady state)
 interval_size = 200    # Number of periods in each subsample interval
 AgentCount = 20000     # Total number of agents to simulate in the economy
