3d6610b2bf8dd7e869f84d59e9a6933d91ff1aaa
edmundcrawley <edmundcrawley@gmail.com>
2018-01-02 09:00:52 -0500

    New File OtherTables.tex creates all tables in paper

--- cAndCwithStickyE/StickyEtools.py
+++ cAndCwithStickyE/StickyEtools.py
@@ -717,7 +717,7 @@ def makeEquilibriumTable(out_filename, four_in_files):
     output += "\\\\ \hline \multicolumn{3}{l}{Cost Of Stickiness}  \n"
     output += " & \multicolumn{2}{c}{999999}  \n"
     output += "  & \multicolumn{2}{c}{9999999} \n"
-    output += " \end{tabular} \\\\  \n"
+    output += " \end{tabular}   \n"
     output += "}  \n"
     output += "\usebox{\EqbmBox}  \n"
     output += "\ifthenelse{\\boolean{StandAlone}}{\\newlength\TableWidth}{}  \n"
