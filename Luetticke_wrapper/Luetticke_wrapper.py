05a5659cf4811fc3581b67eda6a4d84ba5250804
edmundcrawley <edmundcrawley@gmail.com>
2018-05-06 16:27:13 -0400

    Now simulates in steady state

--- Luetticke_wrapper/Luetticke_wrapper.py
+++ Luetticke_wrapper/Luetticke_wrapper.py
@@ -13,13 +13,15 @@ from SteadyStateOneAssetIOUs import SteadyStateOneAssetIOU
 from FluctuationsOneAssetIOUs import FluctuationsOneAssetIOUs, SGU_solver
 from copy import copy, deepcopy
 import numpy as np
+import scipy as sc
+from scipy.interpolate import interp1d
 
 class LuettickeAgent(AgentType):
     '''
     An agent who lives in a LuettickeMarket. This agent has no solve method,
     but instead inherits it from the Luetticke code.
     '''
-    poststate_vars_ = ['aNow','incStateNow']
+    poststate_vars_ = ['bNow','incStateNow']
     
     def __init__(self,AgentCount,seed=0):
         '''
@@ -39,6 +41,11 @@ class LuettickeAgent(AgentType):
         self.track_vars     = []
         self.seed               = seed
         self.resetRNG()
+        self.time_flow = False
+        self.time_vary      = []
+        self.time_inv       = []
+        self.read_shocks    = False
+        self.T_cycle        = 0
         
     def simBirth(self,which_agents):
         '''
@@ -74,6 +81,114 @@ class LuettickeAgent(AgentType):
         self.t_cycle[which_agents] = 0 # Which period of the cycle each agent is currently in
         return None
     
+    def simDeath(self):
+        '''
+        No one dies in Luetticke's model - this function does that.
+        
+        Parameters
+        ----------
+        None
+        
+        Returns
+        -------
+        which_agents : np.array(bool)
+            Boolean array of size AgentCount indicating which agents die.
+        '''
+        which_agents = np.zeros(self.AgentCount, dtype=bool)
+        return which_agents
+    
+    def getShocks(self):
+        '''
+        Finds income state for each agent this period.  
+        
+        Parameters
+        ----------
+        None
+        
+        Returns
+        -------
+        None
+        '''
+        incStatePrev = self.incStateNow
+        incStateNow = np.zeros(self.AgentCount,dtype=int)
+        #base_draws = self.RNG.permutation(np.arange(self.AgentCount,dtype=float)/self.AgentCount + 1.0/(2*self.AgentCount))
+        base_draws = self.RNG.uniform(size=self.AgentCount)
+        Cutoffs = np.cumsum(self.incStateTransition,axis=1)
+        for j in range(self.incStateTransition.shape[0]):
+            these = incStatePrev == j
+            incStateNow[these] = np.searchsorted(Cutoffs[j,:],base_draws[these]).astype(int)
+        self.incStateNow = incStateNow.astype(int)
+        
+    def getStates(self):
+        '''
+        The idiosyncratic states in this model are bNow and incShockNow.
+        These have already been calculated so there is nothing to do here...
+        
+        Parameters
+        ----------
+        None
+        
+        Returns
+        -------
+        None
+        '''
+        return None
+    
+    def getControls(self):
+        '''
+        Calculates consumption for each consumer of this type using the consumption functions.
+        
+        Parameters
+        ----------
+        None
+        
+        Returns
+        -------
+        None
+        '''
+        cNow = np.zeros(self.AgentCount) + np.nan
+        for j in range(self.incStateTransition.shape[0]):
+            these =  j == self.incStateNow
+            cNow[these] = self.SSConsumptionFunc[j](self.bNow[these])
+        self.cNow = cNow
+        return None
+        
+    def getPostStates(self):
+        '''
+        Calculates end-of-period assets for each consumer of this type.
+        
+        Parameters
+        ----------
+        None
+        
+        Returns
+        -------
+        None
+        '''
+        bPrev = self.bNow
+        #For the moment we are only calculating in the steady state - take fixed steady state values below
+        par = self.FluctuationsOneAssetIOU.par
+        RB = par['RB']
+        borrwedge = par['borrwedge']
+        PI = par['PI']
+        RR = (RB+(bPrev.copy()<0.)*borrwedge)/PI
+        W = par['W']
+        N = par['N']
+        H = par['H']
+        Profits = par['PROFITS']
+        profitshare = par['profitshare']
+        hgrid = self.FluctuationsOneAssetIOU.grid['h']
+
+        # Income for agents in each state (including entrepreneur state)
+        income_array = par['gamma']/(1+par['gamma'])*(N/H)*W*hgrid
+        income_array[-1] = Profits*profitshare
+        # Add taxes on all income
+        income_array = par['tau']*income_array
+               
+        self.mNow = bPrev*RR + income_array[self.incStateNow]
+        self.bNow = self.mNow - self.cNow
+        return None
+    
     def getEconomyData(self,Economy):
         '''
         Imports economy-determined objects into self from a Market.
@@ -96,6 +211,7 @@ class LuettickeAgent(AgentType):
         self.m_policy = self.FluctuationsOneAssetIOU.m_policy
         self.numIncStates = self.FluctuationsOneAssetIOU.mpar['nh']
         self.assetGridsize = self.FluctuationsOneAssetIOU.mpar['nm']
+        self.incStateTransition = self.FluctuationsOneAssetIOU.P_H
         #Build steady state consumption function
         SSConsumptionFunc = []
         for j in range(self.numIncStates):
@@ -156,6 +272,7 @@ if __name__ == '__main__':
     from copy import copy
     import pickle
     
+    simulate = True
     solve_ss = False
 
     #First calculate the steady state
@@ -176,4 +293,29 @@ if __name__ == '__main__':
     LuettickeExampleEconomy.solve()
     
     #Simulate
-    
+    if simulate:
+        LuettickeExampleAgent.T_sim = 1000
+        LuettickeExampleAgent.track_vars = ['bNow','cNow','mNow','incStateNow']
+        LuettickeExampleAgent.initializeSim()
+        LuettickeExampleAgent.simulate()
+        
+        
+        ###########################################################
+        # Test code to be removed later
+        meanbNow0 = np.zeros(LuettickeExampleAgent.T_sim)
+        meanbNow1 = np.zeros(LuettickeExampleAgent.T_sim)
+        meanbNow2 = np.zeros(LuettickeExampleAgent.T_sim)
+        meanbNow3 = np.zeros(LuettickeExampleAgent.T_sim)
+        meanbNow = np.zeros(LuettickeExampleAgent.T_sim)
+        for t in range(LuettickeExampleAgent.T_sim):
+            meanbNow0[t] = np.mean(LuettickeExampleAgent.bNow_hist[t,:][LuettickeExampleAgent.incStateNow_hist[t,:]==0])
+            meanbNow1[t] = np.mean(LuettickeExampleAgent.bNow_hist[t,:][LuettickeExampleAgent.incStateNow_hist[t,:]==1])
+            meanbNow2[t] = np.mean(LuettickeExampleAgent.bNow_hist[t,:][LuettickeExampleAgent.incStateNow_hist[t,:]==2])
+            meanbNow3[t] = np.mean(LuettickeExampleAgent.bNow_hist[t,:][LuettickeExampleAgent.incStateNow_hist[t,:]==3])
+            meanbNow[t] = np.mean(LuettickeExampleAgent.bNow_hist[t,:])
+        plt.plot(meanbNow0)
+        plt.plot(meanbNow1)
+        plt.plot(meanbNow2)
+        plt.plot(meanbNow)
+        ###########################################################
+            
