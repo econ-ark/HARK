c33ada931f81165b425416b68a65f708463a01cf
Sumana Harihareswara <sh@changeset.nyc>
2018-05-19 16:44:51 -0400

    Change name to econ-ark which is available on PyPI

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
     # There are some restrictions on what makes a valid project name
     # specification here:
     # https://packaging.python.org/specifications/core-metadata/#name
-    name='HARK',  # Required
+    name='econ-ark',  # Required
 
     # Versions should comply with PEP 440:
     # https://www.python.org/dev/peps/pep-0440/
