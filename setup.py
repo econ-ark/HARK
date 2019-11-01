f7dd5c6198dfdbd4e09229d779908120d1da2b93
sb <sbenthall@gmail.com>
2019-11-01 14:39:03 -0400

    correcting extras_require use; moving numpydoc to dev deps. #429

--- setup.py
+++ setup.py
@@ -148,18 +148,19 @@ setup(
     install_requires=['numpy',
                       'matplotlib',
                       'scipy',
-                      'numpydoc',
                       'dill',
                       'joblib',
                       'funcsigs',
                       'jupyter'
     ],
-    extra_requires=[
-        'future',  # Optional
-        'Sphinx<=1.6.7',
-        'nbsphinx',
-        'recommonmark'
-    ],
+    extras_requires={
+        'dev': [
+            'future',  # Optional
+            'Sphinx<=1.6.7',
+            'nbsphinx',
+            'numpydoc',
+            'recommonmark'
+    ]},
 
     python_requires='>=2.7',
 
