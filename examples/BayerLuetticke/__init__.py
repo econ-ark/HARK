0db0348040be5d04b2069d1a335a1609ebe5e79d
sb <sbenthall@gmail.com>
2019-12-05 17:44:19 -0500

    cleaning up __init__.py

--- examples/BayerLuetticke/__init__.py
+++
@@ -1,6 +0,0 @@
-from BayerLuetticke.Assets.One.FluctuationsOneAssetIOUs import *
-from BayerLuetticke.Assets.One.SteadyStateOneAssetIOUs import *
-from BayerLuetticke.Assets.One.FluctuationsOneAssetIOUsBond import *
-from BayerLuetticke.Assets.One.SteadyStateOneAssetIOUsBond import *
-from BayerLuetticke.Assets.Two.SteadyStateTwoAsset import *
-from BayerLuetticke.Assets.Two.FluctuationsTwoAsset import *
