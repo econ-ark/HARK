eda832307233f8ad7b0c45a40788ed7690f63f6a
Mridul Seth <seth.mridul+github@gmail.com>
2020-02-01 15:03:16 +0530

    Update __init__.py

--- HARK/__init__.py
+++ HARK/__init__.py
@@ -4,7 +4,7 @@ import sys
 if sys.version_info[:2] < (3, 6):
     print(
         "WARNING: Econ-Ark v0.10.4 will be the last supported version for Python %d.%d, "
-        "please upgrade you system to the latest release, read more at https://www.python.org/doc/sunset-python-2/"
+        "please upgrade your system to the latest release, read more at https://www.python.org/doc/sunset-python-2/"
         % sys.version_info[:2]
     )
 
