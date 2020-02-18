960eedac6fda3cf4c18aa721a980f0260f57652a
sb <sbenthall@gmail.com>
2020-02-18 13:16:24 -0500

    putting in temporary dict to keep test passing

--- HARK/ConsumptionSaving/ConsumerParameters.py
+++ HARK/ConsumptionSaving/ConsumerParameters.py
@@ -35,7 +35,25 @@ T_cycle = 1         # Number of periods in the cycle for this agent type
 init_idiosyncratic_shocks = {
     'Rfree' : 1.03,
     'CubicBool' : False,
-    'vFuncBool' : False
+    'vFuncBool' : False,
+    'PermShkStd': [0.1],    # Standard deviation of log permanent income shocks
+    'PermShkCount': 7,      # Number of points in discrete approximation to permanent income shocks
+    'TranShkStd': [0.1],    # Standard deviation of log transitory income shocks
+    'TranShkCount': 7,      # Number of points in discrete approximation to transitory income shocks
+    'UnempPrb': 0.05,       # Probability of unemployment while working
+    'UnempPrbRet': 0.005,   # Probability of "unemployment" while retired
+    'IncUnemp': 0.3,        # Unemployment benefits replacement rate
+    'IncUnempRet': 0.0,     # "Unemployment" benefits when retired
+    'BoroCnstArt': 0.0,     # Artificial borrowing constraint; imposed minimum level of end-of period assets
+    'tax_rate': 0.0,        # Flat income tax rate
+    'T_retire': 0, # Period of retirement (0 --> no retirement)
+    # assets above grid parameters
+    'aXtraMin': 0.001,      # Minimum end-of-period "assets above minimum" value
+    'aXtraMax': 20,         # Maximum end-of-period "assets above minimum" value
+    'aXtraNestFac': 3,      # Exponential nesting factor when constructing "assets above minimum" grid
+    'aXtraCount': 48,       # Number of points in the grid of "assets above minimum"
+    'aXtraExtra': [None],   # Some other value of "assets above minimum" to add to the grid, not used
+    # Income process variables
 }
 
 
