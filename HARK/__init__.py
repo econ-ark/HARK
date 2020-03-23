87ae56f1cd736f311a3d5ba9d58f83f733cf604e
Mridul Seth <seth.mridul+github@gmail.com>
2020-03-23 06:16:51 +0530

    prep work for 0.10.5 release, bump up min_required version to py3.6 (#593)
    * prep work for 0.10.5 release, bump up min_required version to py3.6
    
    * Update __init__.py
    
    * Update __init__.py

--- HARK/__init__.py
+++ HARK/__init__.py
@@ -1,14 +1,3 @@
-from __future__ import absolute_import
-import sys
-
-if sys.version_info[:2] < (3, 6):
-    print(
-        "WARNING: Econ-Ark v0.10.4 will be the last supported version for Python %d.%d, "
-        "please upgrade your system to the latest release, read more at https://www.python.org/doc/sunset-python-2/"
-        % sys.version_info[:2]
-    )
-
-
 from .core import *
 
-__version__ = "0.10.4"
+__version__ = "0.10.5"
