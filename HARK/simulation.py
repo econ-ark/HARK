ed63b059bff05627b1b1c68b7dc44bb8d14a9ff3
sb <sbenthall@gmail.com>
2020-05-14 08:58:27 -0400

    configurable GenericModel; exogenous shocks through distribution class

--- HARK/simulation.py
+++ HARK/simulation.py
@@ -1,12 +1,42 @@
 '''
-Currently empty. Will be used for future simulation handling code.
+Generic simulation.
 '''
 
 from __future__ import division
 import warnings                             # A library for runtime warnings
 from HARK import AgentType
+from HARK.distribution import MeanOneLogNormal
 import networkx as nx
 import numpy as np                          # Numerical Python
+
+
+#####
+# A Sample Configuration
+#####
+sample_configuration = {
+    'params' : {
+        'G' : MeanOneLogNormal(), # income growth SHOCK
+        'R' : 1.1  # rate of return
+    },
+    'states' : {
+        'b' : lambda a_, R: a_ * R,
+        'm' : lambda p_, b : p_ + b, # within-period assets
+    },
+    'controls' : {
+        'c' : lambda m : m / 3 # consumption.
+        # The function is the decision rule.
+    },
+    'post_states' : {
+        'a' : lambda m, c : m - c, #market assets
+        'p' : lambda p_, G: p_ * G # income
+    },
+    'initial_states' : {
+        'p_' : 1,
+        'a_' : 1
+    }
+}
+
+
 
 
 class GenericModel(AgentType):
@@ -21,39 +51,30 @@ class GenericModel(AgentType):
     This class will be oriented towards simulation first.
     Later versions may support generic solvers.
     '''
+    params = {}
+    states = {}
+    controls = {}
+    post_states = {}
+    initial_states = {}
 
-    params = {
-        'G' : 1.1, # income growth factor
-        'R' : 1.1  # rate of return
-        }
-
-    states = {
-        'b' : lambda a_, R: a_ * R,
-        'm' : lambda p_, b : p_ + b, # within-period assets
-        }
-
-    controls = {
-        'c' : lambda m : m / 3 # consumption.
-        # The function is the decision rule.
-        }
-    post_states = {
-        'a' : lambda m, c : m - c, #market assets
-        'p' : lambda p_, G: p_ * G # income
-    }
-    
-    initial_states = {
-        'p_' : 1,
-        'a_' : 1
-        }
-
+    def __init__(self, configuration):
+        self.params = configuration['params']
+        self.states = configuration['states']
+        self.controls = configuration['controls']
+        self.post_states = configuration['post_states']
+        self.initial_states = configuration['initial_states']
 
     ####
     # Kludges for compatibility
-
-    T_sim = 10
-    T_cycle = 0
     AgentCount = 1
+    read_shocks = False
+    seed = 0
     solution = "No solution"
+    T_cycle = 0
+    T_sim = 10
+    poststate_vars = []
+    track_vars = []
+    who_dies_hist = []
     ####
 
 
@@ -79,7 +100,13 @@ class GenericModel(AgentType):
     #   getShocks
 
     def getStates(self):
-        self.agent.states.update(self.params)
+        
+        self.agent.states.update({
+            p : evaluate(self.params[p])
+            for p
+            in self.params})
+
+        print(self.agent.states)
 
         for variable in simulation_order(self.states):
             if variable in self.states:
@@ -144,12 +171,31 @@ class SimulatedAgent():
         self.states[decrement(variable)] = value
         self.update_history(variable, value)
 
+
+def call_function_in_scope(function, context):
+    return function(
+        *[
+            context[name]
+            for name
+            in function.__code__.co_varnames
+        ])
+
 def decrement(var_name):
     '''
     Gets the variable name for this state variable,
     but for the previous time step.
     '''
     return var_name + '_'
+
+def evaluate(process):
+    ## clean this up; should be a type check
+    ## for Distribution, or Distribution should
+    ## have a cleaner interface (like __call__)
+    if hasattr(process, 'draw'):
+        return process.draw(1)[0]
+    else:
+        return process
+
 
 def simulation_order(transitions):
     parents = {
@@ -162,14 +208,6 @@ def simulation_order(transitions):
 
     return [s for s in order if s in transitions]
 
-def call_function_in_scope(function, context):
-    return function(
-        *[
-            context[name]
-            for name
-            in function.__code__.co_varnames
-        ])
-
 
 ################
 # TESTING CODE
@@ -177,7 +215,7 @@ def call_function_in_scope(function, context):
 # once stabilized.
 ################
 
-generic_model_test = GenericModel()
+generic_model_test = GenericModel(sample_configuration)
 generic_model_test.initializeSim()
 generic_model_test.simulate()
 
