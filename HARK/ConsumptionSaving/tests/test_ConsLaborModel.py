54e2359ed2cd75b87dc4a8007df6b252af3a3765
Mridul Seth <seth.mridul+github@gmail.com>
2020-06-25 19:51:52 +0530

    fix typo

--- HARK/ConsumptionSaving/tests/test_ConsLaborModel.py
+++ HARK/ConsumptionSaving/tests/test_ConsLaborModel.py
@@ -8,12 +8,12 @@ import unittest
 class test_LaborIntMargConsumerType(unittest.TestCase):
     def setUp(self):
         self.model = LaborIntMargConsumerType()
-        self.model_finte_lifecycle = LaborIntMargConsumerType(**init_labor_lifecycle)
-        self.model_finte_lifecycle.cycles = 1
+        self.model_finite_lifecycle = LaborIntMargConsumerType(**init_labor_lifecycle)
+        self.model_finite_lifecycle.cycles = 1
 
     def test_solution(self):
         self.model.solve()
-        self.model_finte_lifecycle.solve()
+        self.model_finite_lifecycle.solve()
 
         self.model.T_sim = 120
         self.model.track_vars = ["bNrmNow", "cNrmNow"]
