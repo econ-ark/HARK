1c73465c158b0dd5bd1454880b1a0fc01fb4a670
MateoVG <mateovegi@gmail.com>
2020-07-20 15:40:34 -0400

    separate and label plots

--- HARK/G2EGM/g2egm_example.py
+++ HARK/G2EGM/g2egm_example.py
@@ -39,25 +39,39 @@ import matplotlib.pyplot as plt
 
 
 sol0 = model.solution[18]
+
+
 plt.scatter(model.grids.M, model.grids.N, c=sol0.Pr_1, s=0.15)
+plt.xlabel('M')
+plt.ylabel('N')
+plt.title('Retire?')
+plt.show()
 
-plt.scatter(model.grids.M, model.grids.N, c=sol0.Pr_1, s=0.05)
-
+# Points are in [c,d,v,M,N,best?] order
 plt.scatter(sol0.ws.ucon[3], sol0.ws.ucon[4], c=sol0.ws.ucon[2], s=0.3)
-
+plt.title('Value')
+plt.show()
 plt.scatter(sol0.ws.con[3], sol0.ws.con[4], c=sol0.ws.con[2], s=0.3)
-
+plt.title('Value')
+plt.show()
 plt.scatter(sol0.ws.dcon[3], sol0.ws.dcon[4], c=sol0.ws.dcon[2], s=0.3)
-
+plt.title('Value')
+plt.show()
 plt.scatter(sol0.ws.acon[3], sol0.ws.acon[4], c=sol0.ws.acon[2], s=0.3)
-
+plt.title('Value')
+plt.show()
 
 
 sol = model.solution[4]
-plt.scatter(model.grids.mMesh, model.grids.nMesh, c=sol.Pr_1, s=0.3)
+plt.scatter(model.grids.M, model.grids.N, c=sol.Pr_1, s=0.15)
+plt.xlabel('M')
+plt.ylabel('N')
+plt.title('Retire?')
+plt.show()
 
-
-plt.scatter(model.grids.mMesh, model.grids.nMesh, c=sol.Pr_1, s=0.3)
-plt.scatter(model.grids.mMesh, model.grids.nMesh, c=sol.ws.acon[5], s=0.3, alpha=0.1)
-
+plt.scatter(model.grids.M, model.grids.N, c=sol.ws.acon[5], s=0.3, alpha=1)
+plt.xlabel('M')
+plt.ylabel('N')
+plt.title('Is acon best?')
+plt.show()
 
