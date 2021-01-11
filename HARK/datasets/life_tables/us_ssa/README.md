bb9013325896fb2c554e943b5e6ae8396c687bad
MateoVG <mateovegi@gmail.com>
2021-01-11 11:20:40 -0500

    Import SSA tools

---
+++ HARK/datasets/life_tables/us_ssa/README.md
@@ -0,0 +1,40 @@
+# United States Social Security Administration Life-Tables
+
+This folder contains tools for producing sequences of life-cycle survival probabilities for microeconomic
+models from official U.S. mortality estimates.
+
+## Sources
+
+The life tables contained in this folder correspond to the downloadable
+''Period Life Tables'' from the 2020 Annual Trustees Report of the SSA.
+
+They were downloaded from [this link](https://www.ssa.gov/oact/HistEst/PerLifeTables/2020/PerLifeTables2020.html)
+on January 7, 2021.
+
+## Format
+
+There are four `.csv` files:
+- `PerLifeTables_F_Hist_TR2020.csv` contains historical (1900-2017) information for females.
+- `PerLifeTables_F_Alt2_TR2020.csv` contains projected (2018-2095) information for females.
+- `PerLifeTables_M_Hist_TR2020.csv` contains historical (1900-2017) information for males.
+- `PerLifeTables_M_Alt2_TR2020.csv` contains projected (2018-2095) information for males.
+
+All the tables have the same format. There are three columns of interest for our purposes:
+- `Year`.
+- `x`: age.
+- `q(x)`: probability of death.
+
+As an example, the probability that a male who was 27 years old in 1990 would die within a year (from 1990 to 1991) is found in file `PerLifeTables_M_Hist_TR2020.csv`, in column `q(x)` and the row in which `Year == 1990` and `x == 27`.
+
+Visit the [SSA's site](https://www.ssa.gov/oact/HistEst/PerLifeTables/2020/PerLifeTables2020.html) for a complete description of the tables.
+
+## Usage
+
+`SSATools.py` contains functions that translate the information in the `.csv`
+life-tables into sequences of survival probabilities in the format that HARK's
+life-cycle models require.
+
+The main function is `parse_ssa_life_table`, which produces survival
+probabilities for a given sex, age-range, and year of birth. See the function's
+documentation for details. `examples/Calibration/Calib_plots.py` contains
+examples of its use.
---
+++ HARK/datasets/life_tables/us_ssa/SSATools.py
@@ -0,0 +1,131 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan  8 15:36:14 2021
+
+@author: Mateo
+"""
+
+import numpy as np
+import pandas as pd
+from warnings import warn
+import os
+
+ssa_tables_dir = os.path.dirname(os.path.abspath(__file__))
+
+def parse_ssa_life_table(min_age, max_age, female = True,
+                         cohort = None, cross_sec = False, year = None):
+    """
+    Reads (year,age)-specifc death probabilities form SSA life tables and
+    transforms them to a list of survival probabilities in the format that
+    HARK expects.
+    
+    Two methods are supported:
+        - Cross-sectional: finds the 1-year survival probabilities for
+          individuals in the age range for a fixed year.
+          In the output,
+          SurvPrb(age) = 1 - DeathPrb(age, year)
+          
+        - Longitudinal: finds the 1-year survival probabilities for individuals
+          of a fixed cohort at different ages (and years). 
+          In the output,
+          SurvPrb(age) = 1 - DeathPrb(age, cohort + age)
+    
+    Parameters
+    ----------
+    min_age : int
+        Minimum age for survival probabilities.
+    max_age : int
+        Maximum age for survival probabilities.
+    female : bool, optional
+        Boolean indicating wether to use female or male survival probabilities.
+        The default is True (female).
+    cohort : int, optional
+        If longitudinal probabilities are requested, this is the birth year of
+        the cohort that will be tracked. The default is None.
+    cross_sec : bool, optional
+        Boolean indicating whether the cross-sectional method should be used.
+        The default is False (using the longitudinal method).
+    year : int, optional
+        If cross-sectional probabilities are requestedm this is the year at
+        which they will be taken. The default is None.
+        
+    Returns
+    -------
+    LivPrb : [float]
+        List of 1-year survival probabilities.
+        LivPrb[n] corresponds to the probability that an indivivual of age
+        'min_age' + n survives one year, in the year 'year' if the
+        cross-sectional method is used or 'cohort' + ('min_age' + n) if the
+        longitudinal method is used.
+        
+    """
+    
+    
+    # Infix for file name depending on sex
+    abb = 'F' if female else 'M'
+    
+    # Find year - age combinations that we need
+    assert max_age >= min_age, 'The maximum age can not be lower than the minimum age.'
+    ages  = np.arange(min_age, max_age + 1)
+    
+    if cross_sec:
+        
+        if year is None:
+            raise(TypeError('You must provide a year when using ' + 
+                            'cross-sectional survival probabilities.'))
+        
+        years = np.repeat(year, ages.shape)
+        
+    else:
+        
+        if cohort is None:
+            raise(TypeError('You must provide a cohort (birth year) when ' + 
+                            'using longitudinal survival probabilities.'))
+        
+        years = cohort + ages
+    
+    # Create filenames
+    
+    # Historical and forecasted
+    file_hist = os.path.join(ssa_tables_dir, 'PerLifeTables_'+abb+'_Hist_TR2020.csv')
+    file_fore = os.path.join(ssa_tables_dir, 'PerLifeTables_'+abb+'_Alt2_TR2020.csv')
+    
+    # Read them
+    hist_tab = pd.read_csv(file_hist, sep = ',', skiprows=4,
+                           usecols=['Year','x','q(x)'], index_col = ['Year','x'])
+    fore_tab = pd.read_csv(file_fore, sep = ',', skiprows=4,
+                           usecols=['Year','x','q(x)'], index_col = ['Year','x'])
+    
+    # Find the point at which projections start
+    max_hist = max(hist_tab.index.get_level_values('Year'))
+    
+    # Warn the user if projections are used.
+    if max(years) > max_hist:
+        message = 'Survival probabilities beyond {} are projections.'.format(max_hist)
+        warn(message)
+        
+    # Concatenate them
+    tab = pd.concat([hist_tab, fore_tab])
+    
+    # Subset and sort deathrates.
+    
+    message = ('Parsed life tables do not contain all the requested ' +
+               'age-year combinations.')
+    try:
+        
+        DeathPrb = tab.loc[zip(years,ages)].sort_values(by = 'x')
+        
+    except KeyError as e:
+        
+        raise Exception(message).with_traceback(e.__traceback__)
+    
+    # Transform to numpy survival probabilities
+    LivPrb = 1 - DeathPrb['q(x)'].to_numpy()
+    
+    # Make sure we got all the probabilities
+    assert len(LivPrb) == max_age - min_age + 1, message
+    
+    # Transform from array to list
+    LivPrb = list(LivPrb)
+    
+    return LivPrb
