e6dcd921a9d2363b2d5446ace85e3d24cd4de8be
MateoVG <mateovegi@gmail.com>
2021-01-14 10:15:35 -0500

    Lint using "black"

--- HARK/datasets/life_tables/us_ssa/SSATools.py
+++ HARK/datasets/life_tables/us_ssa/SSATools.py
@@ -12,6 +12,7 @@ import os
 
 ssa_tables_dir = os.path.dirname(os.path.abspath(__file__))
 
+
 def get_ssa_life_tables():
     """
     Reads all the SSA life tables and combines them, adding columns indicating
@@ -27,28 +28,31 @@ def get_ssa_life_tables():
     """
     # Read the four tables and add columns identifying them
     dsets = []
-    for sex in ['M','F']:
-        for method in ['Historical', 'Projected']:
-            
+    for sex in ["M", "F"]:
+        for method in ["Historical", "Projected"]:
+
             # Construct file name
-            infix = 'Hist' if method == 'Historical' else 'Alt2'
-            filename = os.path.join(ssa_tables_dir,
-                                    'PerLifeTables_'+sex+'_'+infix+'_TR2020.csv')
-            
+            infix = "Hist" if method == "Historical" else "Alt2"
+            filename = os.path.join(
+                ssa_tables_dir, "PerLifeTables_" + sex + "_" + infix + "_TR2020.csv"
+            )
+
             # Read csv
-            table = pd.read_csv(filename, sep = ',', skiprows=4)
-            
+            table = pd.read_csv(filename, sep=",", skiprows=4)
+
             # Add identifying info
-            table['Sex'] = sex
-            table['Method'] = method
-            
+            table["Sex"] = sex
+            table["Method"] = method
+
             dsets.append(table)
-    
+
     # Concatenate tables by row and return them
     return pd.concat(dsets)
-    
-def parse_ssa_life_table(min_age, max_age, female = True,
-                         cohort = None, cross_sec = False, year = None):
+
+
+def parse_ssa_life_table(
+    min_age, max_age, female=True, cohort=None, cross_sec=False, year=None
+):
     """
     Reads (year,age)-specifc death probabilities form SSA life tables and
     transforms them to a list of survival probabilities in the format that
@@ -94,73 +98,96 @@ def parse_ssa_life_table(min_age, max_age, female = True,
         longitudinal method is used.
         
     """
-    
-    
+
     # Infix for file name depending on sex
-    abb = 'F' if female else 'M'
-    
+    abb = "F" if female else "M"
+
     # Find year - age combinations that we need
-    assert max_age >= min_age, 'The maximum age can not be lower than the minimum age.'
-    ages  = np.arange(min_age, max_age + 1)
-    
+    assert max_age >= min_age, "The maximum age can not be lower than the minimum age."
+    ages = np.arange(min_age, max_age + 1)
+
     if cross_sec:
-        
+
         if year is None:
-            raise(TypeError('You must provide a year when using ' + 
-                            'cross-sectional survival probabilities.'))
-        
+            raise (
+                TypeError(
+                    "You must provide a year when using "
+                    + "cross-sectional survival probabilities."
+                )
+            )
+
         years = np.repeat(year, ages.shape)
-        
+
     else:
-        
+
         if cohort is None:
-            raise(TypeError('You must provide a cohort (birth year) when ' + 
-                            'using longitudinal survival probabilities.'))
-        
+            raise (
+                TypeError(
+                    "You must provide a cohort (birth year) when "
+                    + "using longitudinal survival probabilities."
+                )
+            )
+
         years = cohort + ages
-    
+
     # Create filenames
-    
+
     # Historical and forecasted
-    file_hist = os.path.join(ssa_tables_dir, 'PerLifeTables_'+abb+'_Hist_TR2020.csv')
-    file_fore = os.path.join(ssa_tables_dir, 'PerLifeTables_'+abb+'_Alt2_TR2020.csv')
-    
+    file_hist = os.path.join(
+        ssa_tables_dir, "PerLifeTables_" + abb + "_Hist_TR2020.csv"
+    )
+    file_fore = os.path.join(
+        ssa_tables_dir, "PerLifeTables_" + abb + "_Alt2_TR2020.csv"
+    )
+
     # Read them
-    hist_tab = pd.read_csv(file_hist, sep = ',', skiprows=4,
-                           usecols=['Year','x','q(x)'], index_col = ['Year','x'])
-    fore_tab = pd.read_csv(file_fore, sep = ',', skiprows=4,
-                           usecols=['Year','x','q(x)'], index_col = ['Year','x'])
-    
+    hist_tab = pd.read_csv(
+        file_hist,
+        sep=",",
+        skiprows=4,
+        usecols=["Year", "x", "q(x)"],
+        index_col=["Year", "x"],
+    )
+    fore_tab = pd.read_csv(
+        file_fore,
+        sep=",",
+        skiprows=4,
+        usecols=["Year", "x", "q(x)"],
+        index_col=["Year", "x"],
+    )
+
     # Find the point at which projections start
-    max_hist = max(hist_tab.index.get_level_values('Year'))
-    
+    max_hist = max(hist_tab.index.get_level_values("Year"))
+
     # Warn the user if projections are used.
     if max(years) > max_hist:
-        message = 'Survival probabilities beyond {} are projections.'.format(max_hist)
+        message = "Survival probabilities beyond {} are projections.".format(max_hist)
         warn(message)
-        
+
     # Concatenate them
     tab = pd.concat([hist_tab, fore_tab])
-    
+
     # Subset and sort deathrates.
-    
-    message = ('Parsed life tables do not contain all the requested ' +
-               'age-year combinations.')
+
+    message = (
+        "Parsed life tables do not contain all the requested "
+        + "age-year combinations."
+    )
     try:
-        
-        DeathPrb = tab.loc[zip(years,ages)].sort_values(by = 'x')
-        
+
+        DeathPrb = tab.loc[zip(years, ages)].sort_values(by="x")
+
     except KeyError as e:
-        
+
         raise Exception(message).with_traceback(e.__traceback__)
-    
+
     # Transform to numpy survival probabilities
-    LivPrb = 1 - DeathPrb['q(x)'].to_numpy()
-    
+    LivPrb = 1 - DeathPrb["q(x)"].to_numpy()
+
     # Make sure we got all the probabilities
     assert len(LivPrb) == max_age - min_age + 1, message
-    
+
     # Transform from array to list
     LivPrb = list(LivPrb)
-    
+
     return LivPrb
