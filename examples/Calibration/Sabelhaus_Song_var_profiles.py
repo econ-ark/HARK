58029d8143ad60700a4ca527c5fd86fc8344e568
MateoVG <mateovegi@gmail.com>
2021-01-18 16:46:27 -0500

    Reformat example

--- examples/Calibration/Sabelhaus_Song_var_profiles.py
+++ examples/Calibration/Sabelhaus_Song_var_profiles.py
@@ -3,29 +3,47 @@
 Created on Thu Jan 14 16:44:09 2021
 
 @author: Mateo
+
+This short script demonstrates how to use the module for computing
+[1] Sabelhaus & Song (2010) age profiles of income volatility.
+It does so by replicating the results from the original paper (Figure 6 in [1])
+
+[1] Sabelhaus, J., & Song, J. (2010). The great moderation in micro labor
+    earnings. Journal of Monetary Economics, 57(4), 391-403.
+
 """
 
-import numpy as np
 import matplotlib.pyplot as plt
 from HARK.Calibration.Income.SabelhausSongProfiles import sabelhaus_song_var_profile
 
+# Set up ages and cohorts at which we will get the variances
 age_min = 27
 age_max = 54
-ages = np.arange(age_min, age_max + 1)
-years = [1940, 1965, None]
+cohorts = [1940, 1965, None]
 
+# Find volatility profiles using the module
 variances = [sabelhaus_song_var_profile(age_min = age_min,
-                                        age_max = age_max, cohort = y)
-             for y in years]
+                                        age_max = age_max, cohort = c)
+             for c in cohorts]
 
-# Plot transitory variances
+# %% Plots
+
+# Plot transitory shock variances
 plt.figure()
-for i in range(len(years)):
-    plt.plot(ages, variances[i]['TranShkStd'], label = 'Tran. {} cohort'.format(years[i]))
+for i in range(len(cohorts)):
+    
+    coh_label = 'aggregate' if cohorts[i] is None else cohorts[i]
+    plt.plot(variances[i]['Age'], variances[i]['TranShkStd'],
+             label = 'Tran. {} cohort'.format(coh_label))
+    
 plt.legend()
 
-# Plot permanent variances
+# Plot permanent shock variances
 plt.figure()
-for i in range(len(years)):
-    plt.plot(ages, variances[i]['PermShkStd'], label = 'Perm. {} cohort'.format(years[i]))
+for i in range(len(cohorts)):
+    
+    coh_label = 'aggregate' if cohorts[i] is None else cohorts[i]
+    plt.plot(variances[i]['Age'], variances[i]['PermShkStd'],
+             label = 'Perm. {} cohort'.format(coh_label))
+    
 plt.legend()
