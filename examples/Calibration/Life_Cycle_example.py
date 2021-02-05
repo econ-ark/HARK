a6fa62ebc0e2a833b1c0a237382976a0a0603fcc
Mateo Velasquez-Giraldo <mateovegi@gmail.com>
2021-02-05 16:08:17 -0500

    Improve and format LC example

--- examples/Calibration/Life_Cycle_example.py
+++ examples/Calibration/Life_Cycle_example.py
@@ -15,15 +15,14 @@
 
 # %%
 from HARK.ConsumptionSaving.ConsIndShockModel import (
-IndShockConsumerType,
-init_lifecycle
+    IndShockConsumerType,
+    init_lifecycle,
 )
 
-from HARK.Calibration.Calibration import (
+from HARK.Calibration.Income.IncomeTools import (
     ParseIncomeSpec,
     ParseTimeParams,
     CGM_income,
-    Cagetti_income
 )
 
 from HARK.datasets.life_tables.us_ssa.SSATools import parse_ssa_life_table
@@ -35,29 +34,39 @@ from copy import copy
 # %% Alter calibration
 birth_age = 25
 death_age = 90
+adjust_infl_to = 1992
 income_calib = CGM_income
-education = 'College'
+education = "College"
 
 # Income specification
-income_params = ParseIncomeSpec(age_min = birth_age, age_max = death_age,
-                                **income_calib[education], SabelhausSong=True)
+income_params = ParseIncomeSpec(
+    age_min=birth_age,
+    age_max=death_age,
+    adjust_infl_to=adjust_infl_to,
+    **income_calib[education],
+    SabelhausSong=True
+)
 
 # Initial distribution of wealth and permanent income
-dist_params = income_wealth_dists_from_scf(base_year=1992, age = birth_age,
-                                           education = education, wave = 1995)
+dist_params = income_wealth_dists_from_scf(
+    base_year=adjust_infl_to, age=birth_age, education=education, wave=1995
+)
 
 # We need survival probabilities only up to death_age-1, because survival
 # probability at death_age is 1.
-liv_prb = parse_ssa_life_table(female = True, cross_sec = True, year = 2004,
-                               min_age = birth_age, max_age = death_age - 1)
+liv_prb = parse_ssa_life_table(
+    female=True, cross_sec=True, year=2004, min_age=birth_age, max_age=death_age - 1
+)
 
-time_params = ParseTimeParams(age_birth = birth_age, age_death = death_age)
+# Parameters related to the number of periods implied by the calibration
+time_params = ParseTimeParams(age_birth=birth_age, age_death=death_age)
 
+# Update all the new parameters
 params = copy(init_lifecycle)
 params.update(time_params)
 params.update(dist_params)
 params.update(income_params)
-params.update({'LivPrb': liv_prb})
+params.update({"LivPrb": liv_prb})
 
 # %% Create and solve agent
 Agent = IndShockConsumerType(**params)
@@ -65,14 +74,12 @@ Agent.solve()
 
 # %% Simulation
 
-# Setup
-
 # Number of agents and periods in the simulation.
 Agent.AgentCount = 500
-Agent.T_sim      = 200
+Agent.T_sim = 200
 
 # Set up the variables we want to keep track of.
-Agent.track_vars = ['aNrmNow','cNrmNow', 'pLvlNow', 't_age','mNrmNow']
+Agent.track_vars = ["aNrmNow", "cNrmNow", "pLvlNow", "t_age", "mNrmNow"]
 
 # Run the simulations
 Agent.initializeSim()
@@ -80,29 +87,28 @@ Agent.simulate()
 
 # %% Extract and format simulation results
 
-raw_data = {'Age': Agent.history['t_age'].flatten()+birth_age - 1,
-            'pIncome': Agent.history['pLvlNow'].flatten(),
-            'nrmM': Agent.history['mNrmNow'].flatten(),
-            'nrmC': Agent.history['cNrmNow'].flatten()}
+raw_data = {
+    "Age": Agent.history["t_age"].flatten() + birth_age - 1,
+    "pIncome": Agent.history["pLvlNow"].flatten(),
+    "nrmM": Agent.history["mNrmNow"].flatten(),
+    "nrmC": Agent.history["cNrmNow"].flatten(),
+}
 
 Data = pd.DataFrame(raw_data)
-Data['Cons'] = Data.nrmC * Data.pIncome
-Data['M'] = Data.nrmM * Data.pIncome
+Data["Cons"] = Data.nrmC * Data.pIncome
+Data["M"] = Data.nrmM * Data.pIncome
 
 # %% Plots
 
 # Find the mean of each variable at every age
-AgeMeans = Data.groupby(['Age']).mean().reset_index()
+AgeMeans = Data.groupby(["Age"]).median().reset_index()
 
 plt.figure()
-plt.plot(AgeMeans.Age, AgeMeans.pIncome,
-         label = 'Permanent Income')
-plt.plot(AgeMeans.Age, AgeMeans.M,
-         label = 'Market resources')
-plt.plot(AgeMeans.Age, AgeMeans.Cons,
-         label = 'Consumption')
+plt.plot(AgeMeans.Age, AgeMeans.pIncome, label="Permanent Income")
+plt.plot(AgeMeans.Age, AgeMeans.M, label="Market resources")
+plt.plot(AgeMeans.Age, AgeMeans.Cons, label="Consumption")
 plt.legend()
-plt.xlabel('Age')
-plt.ylabel('Thousands of 1992 USD')
-plt.title('Variable Means Conditional on Survival')
+plt.xlabel("Age")
+plt.ylabel("Thousands of {} USD".format(adjust_infl_to))
+plt.title("Variable Medians Conditional on Survival")
 plt.grid()
