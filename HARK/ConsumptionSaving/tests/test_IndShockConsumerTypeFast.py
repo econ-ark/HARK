245fc046c181dc55f4de7516749b48b521feea3a
Mateo Velasquez-Giraldo <mateovegi@gmail.com>
2021-02-12 17:21:50 -0500

    Update tests

--- HARK/ConsumptionSaving/tests/test_IndShockConsumerTypeFast.py
+++ HARK/ConsumptionSaving/tests/test_IndShockConsumerTypeFast.py
@@ -37,20 +37,20 @@ class testIndShockConsumerTypeFast(unittest.TestCase):
         LifecycleExample.solve()
 
         # test the solution_terminal
-        self.assertAlmostEqual(LifecycleExample.solution[10].cFunc(2).tolist(), 2)
+        self.assertAlmostEqual(LifecycleExample.solution[-1].cFunc(2).tolist(), 2)
 
-        self.assertAlmostEqual(LifecycleExample.solution[9].cFunc(1), 0.97769632)
-        self.assertAlmostEqual(LifecycleExample.solution[8].cFunc(1), 0.96624445)
-        self.assertAlmostEqual(LifecycleExample.solution[7].cFunc(1), 0.95691449)
+        self.assertAlmostEqual(LifecycleExample.solution[9].cFunc(1), 0.89066194)
+        self.assertAlmostEqual(LifecycleExample.solution[8].cFunc(1), 0.89144313)
+        self.assertAlmostEqual(LifecycleExample.solution[7].cFunc(1), 0.89210133)
 
         self.assertAlmostEqual(
-            LifecycleExample.solution[0].cFunc(1).tolist(), 0.87362789
+            LifecycleExample.solution[0].cFunc(1).tolist(), 0.8928547282397321
         )
         self.assertAlmostEqual(
-            LifecycleExample.solution[1].cFunc(1).tolist(), 0.9081621
+            LifecycleExample.solution[1].cFunc(1).tolist(), 0.8930303445748624
         )
         self.assertAlmostEqual(
-            LifecycleExample.solution[2].cFunc(1).tolist(), 0.9563899
+            LifecycleExample.solution[2].cFunc(1).tolist(), 0.8933075371183773
         )
 
     def test_simulated_values(self):
