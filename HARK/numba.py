a6edc3463f964aed16934a9e11e7ddce50b69caf
Alan Lujan <alanlujan91@gmail.com>
2021-05-03 14:54:26 -0400

    add LinearInterpFast
    LinearInterpFast is a numba implementation of LinearInterp; see example notebook for speed comparisons

--- HARK/numba.py
+++ HARK/numba.py
@@ -1,5 +1,6 @@
 import numpy as np
-from numba import njit, vectorize
+from numba import njit, vectorize, int32, float64, boolean, types, optional
+from numba.experimental import jitclass
 
 from HARK.utilities import (
     CRRAutility,
@@ -18,6 +19,147 @@ CRRAutilityP_inv = vectorize(CRRAutilityP_inv, cache=True)
 CRRAutility_invP = vectorize(CRRAutility_invP, cache=True)
 CRRAutility_inv = vectorize(CRRAutility_inv, cache=True)
 CRRAutilityP_invP = vectorize(CRRAutilityP_invP, cache=True)
+
+linear_specs = {
+    "x_list": float64[::1],
+    "y_list": float64[::1],
+    "intercept_limit": optional(float64),
+    "slope_limit": optional(float64),
+    "lower_extrap": boolean,
+    "distance_criteria": types.ListType(types.string),
+    "x_n": int32,
+    "decay_extrap": boolean,
+    "decay_extrap_A": float64,
+    "decay_extrap_B": float64,
+}
+
+
+@jitclass(linear_specs)
+class LinearInterpFast(object):
+    """
+    A "from scratch" 1D linear interpolation class.  Allows for linear or decay
+    extrapolation (approaching a limiting linear function from below).
+
+    NOTE: When no input is given for the limiting linear function, linear
+    extrapolation is used above the highest gridpoint.
+
+    Parameters
+    ----------
+    x_list : np.array
+        List of x values composing the grid.
+    y_list : np.array
+        List of y values, representing f(x) at the points in x_list.
+    intercept_limit : float
+        Intercept of limiting linear function.
+    slope_limit : float
+        Slope of limiting linear function.
+    lower_extrap : boolean
+        Indicator for whether lower extrapolation is allowed.  False means
+        f(x) = NaN for x < min(x_list); True means linear extrapolation.
+    """
+
+    # Class members are not yet supported in jitclass: distance_criteria is moved to object attribute
+
+    def __init__(
+        self, x_list, y_list, intercept_limit=None, slope_limit=None, lower_extrap=False
+    ):
+        # self.distance_criteria = typed.List(["x_list", "y_list"])
+        self.x_list = x_list  # checks are not possible, must already be flat array
+        self.y_list = y_list  # checks are not possible, must already be flat array
+
+        self.lower_extrap = lower_extrap
+        self.x_n = self.x_list.size
+
+        # Make a decay extrapolation
+        if intercept_limit is not None and slope_limit is not None:
+            slope_at_top = (y_list[-1] - y_list[-2]) / (x_list[-1] - x_list[-2])
+            level_diff = intercept_limit + slope_limit * x_list[-1] - y_list[-1]
+            slope_diff = slope_limit - slope_at_top
+            # If the model that can handle uncertainty has been calibrated with
+            # with uncertainty set to zero, the 'extrapolation' will blow up
+            # Guard against that and nearby problems by testing slope equality
+            if not np.isclose(slope_limit, slope_at_top, atol=1e-15):
+                self.decay_extrap_A = level_diff
+                self.decay_extrap_B = -slope_diff / level_diff
+                self.intercept_limit = intercept_limit
+                self.slope_limit = slope_limit
+                self.decay_extrap = True
+            else:
+                self.decay_extrap = False
+        else:
+            self.decay_extrap = False
+
+    def _get_index(self, x):
+        return np.maximum(np.searchsorted(self.x_list[:-1], x), 1)
+
+    def _evaluate(self, x, i):
+        """
+        Returns the level of the interpolated function at each value in x.  Only
+        called internally by HARKinterpolator1D.__call__ (etc).
+        """
+
+        alpha = (x - self.x_list[i - 1]) / (self.x_list[i] - self.x_list[i - 1])
+        y = (1.0 - alpha) * self.y_list[i - 1] + alpha * self.y_list[i]
+
+        if not self.lower_extrap:
+            below_lower_bound = x < self.x_list[0]
+            y[below_lower_bound] = np.nan
+
+        if self.decay_extrap:
+            above_upper_bound = x > self.x_list[-1]
+            x_temp = x[above_upper_bound] - self.x_list[-1]
+            y[above_upper_bound] = (
+                self.intercept_limit
+                + self.slope_limit * x[above_upper_bound]
+                - self.decay_extrap_A * np.exp(-self.decay_extrap_B * x_temp)
+            )
+
+        return y
+
+    def _der(self, x, i):
+        """
+        Returns the first derivative of the interpolated function at each value
+        in x. Only called internally by HARKinterpolator1D.derivative (etc).
+        """
+
+        dydx = (self.y_list[i] - self.y_list[i - 1]) / (
+            self.x_list[i] - self.x_list[i - 1]
+        )
+
+        if not self.lower_extrap:
+            below_lower_bound = x < self.x_list[0]
+            dydx[below_lower_bound] = np.nan
+
+        if self.decay_extrap:
+            above_upper_bound = x > self.x_list[-1]
+            x_temp = x[above_upper_bound] - self.x_list[-1]
+
+            dydx[
+                above_upper_bound
+            ] = self.slope_limit + self.decay_extrap_B * self.decay_extrap_A * np.exp(
+                -self.decay_extrap_B * x_temp
+            )
+
+        return dydx
+
+    def eval(self, x):
+        i = self._get_index(x)
+        y = self._evaluate(x, i)
+
+        return y
+
+    def derivative(self, x):
+        i = self._get_index(x)
+        dydx = self._der(x, i)
+
+        return dydx
+
+    def eval_with_derivative(self, x):
+        i = self._get_index(x)
+        y = self._evaluate(x, i)
+        dydx = self._der(x, i)
+
+        return y, dydx
 
 
 @njit(cache=True, error_model="numpy")
