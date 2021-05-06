26941f12bf5e2a34e2d9264da96728afd1730ff8
Mridul Seth <seth.mridul+github@gmail.com>
2021-05-06 17:35:23 +0200

    make tests a bit less sensitive

--- HARK/ConsumptionSaving/tests/test_ConsAggShockModel.py
+++ HARK/ConsumptionSaving/tests/test_ConsAggShockModel.py
@@ -54,7 +54,7 @@ class testAggShockConsumerType(unittest.TestCase):
             self.economy.AFunc.slope, 1.116259456228145
         )
 
-        self.assertAlmostEqual(self.economy.history["MaggNow"][10], 7.456324335623432)
+        self.assertAlmostEqual(self.economy.history["MaggNow"][10], 7.45632)
 
 
 class testAggShockMarkovConsumerType(unittest.TestCase):
@@ -90,7 +90,7 @@ class testAggShockMarkovConsumerType(unittest.TestCase):
             self.economy.AFunc[0].slope, 1.0904698841958917
         )
 
-        self.assertAlmostEqual(self.economy.history["AaggNow"][5], 9.467758924955897)
+        self.assertAlmostEqual(self.economy.history["AaggNow"][5], 9.46775)
 
 
 class KrusellSmithTestCase(unittest.TestCase):
