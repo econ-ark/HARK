c04144de39515c7e3166c3c9de44bb9aa4ca3fad
alanlujan91 <alanlujan91@gmail.com>
2022-02-23 21:57:45 -0500

    Rfree not time varying yet

--- HARK/ConsumptionSaving/ConsRiskyAssetModel.py
+++ HARK/ConsumptionSaving/ConsRiskyAssetModel.py
@@ -45,8 +45,6 @@ class RiskyAssetIndShockConsumerType(IndShockConsumerType):
     """
 
     shock_vars_ = IndShockConsumerType.shock_vars_ + ["Adjust", "Risky"]
-    
-    time_vary_ = IndShockConsumerType.time_vary_ + ["Rfree"]
 
     def __init__(self, verbose=False, quiet=False, **kwds):
         params = init_risky_asset.copy()
@@ -120,7 +118,7 @@ class RiskyAssetIndShockConsumerType(IndShockConsumerType):
             self.RiskyDstn = IndexDistribution(
                 Lognormal.from_mean_std,
                 {"mean": self.RiskyAvg, "std": self.RiskyStd},
-                seed=self.RNG.randint(0, 2**31 - 1),
+                seed=self.RNG.randint(0, 2 ** 31 - 1),
             ).approx(self.RiskyCount)
 
             self.add_to_time_vary("RiskyDstn")
@@ -258,7 +256,7 @@ class RiskyAssetIndShockConsumerType(IndShockConsumerType):
             RiskyStd = self.RiskyStd
 
         self.shocks["Risky"] = Lognormal.from_mean_std(
-            RiskyAvg, RiskyStd, seed=self.RNG.randint(0, 2**31 - 1)
+            RiskyAvg, RiskyStd, seed=self.RNG.randint(0, 2 ** 31 - 1)
         ).draw(1)
 
     def get_Adjust(self):
@@ -276,7 +274,7 @@ class RiskyAssetIndShockConsumerType(IndShockConsumerType):
         None
         """
         self.shocks["Adjust"] = IndexDistribution(
-            Bernoulli, {"p": self.AdjustPrb}, seed=self.RNG.randint(0, 2**31 - 1)
+            Bernoulli, {"p": self.AdjustPrb}, seed=self.RNG.randint(0, 2 ** 31 - 1)
         ).draw(self.t_cycle)
 
     def initialize_sim(self):
@@ -319,8 +317,8 @@ class RiskyAssetIndShockConsumerType(IndShockConsumerType):
 RiskyAssetConsumerType = RiskyAssetIndShockConsumerType
 
 
-class RiskyReturnGivenFixedPortfolioShareConsumerType(RiskyAssetConsumerType):
-    time_vary_ = RiskyAssetConsumerType.time_vary_ + ["RiskyShareFixed"]
+class RiskyReturnGivenFixedPortfolioShareConsumerType(RiskyAssetIndShockConsumerType):
+    time_vary_ = RiskyAssetIndShockConsumerType.time_vary_ + ["RiskyShareFixed"]
 
     def __init__(self, verbose=False, quiet=False, **kwds):
         params = init_risky_share_fixed.copy()
@@ -328,7 +326,9 @@ class RiskyReturnGivenFixedPortfolioShareConsumerType(RiskyAssetConsumerType):
         kwds = params
 
         # Initialize a basic consumer type
-        RiskyAssetConsumerType.__init__(self, verbose=verbose, quiet=quiet, **kwds)
+        RiskyAssetIndShockConsumerType.__init__(
+            self, verbose=verbose, quiet=quiet, **kwds
+        )
 
         self.solve_one_period = make_one_period_oo_solver(
             ConsFixedPortfolioRiskyAssetIndShkSolver
@@ -504,8 +504,7 @@ class ConsRiskyAssetIndShkSolver(ConsIndShockSolver):
             # if zero is BoroCnstNat, do not evaluate at 0.0
             aNrmNow = self.aXtraGrid
             bNrmNext = np.append(
-                aNrmNow[0] * self.RiskyDstn.X.min(),
-                aNrmNow * self.RiskyDstn.X.max(),
+                aNrmNow[0] * self.RiskyDstn.X.min(), aNrmNow * self.RiskyDstn.X.max(),
             )
             wNrmNext = np.append(
                 bNrmNext[0] / (self.PermGroFac * self.PermShkDstn.X.max()),
