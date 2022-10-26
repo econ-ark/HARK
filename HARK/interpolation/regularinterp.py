d591fb2c096c27933a8ee199f98a78f269060548
Alan Lujan <alanlujan91@gmail.com>
2022-10-26 10:30:14 -0400

    implement regularized interpolators

--- HARK/interpolation/regularinterp.py
+++ HARK/interpolation/regularinterp.py
@@ -145,3 +145,81 @@ def nb_interp(grids, args, coordinates):
         coordinates[dim] = np.interp(new_args, arg_grid, np.arange(arg_grid.size))
 
     return coordinates
+class RegularizedPolynomialInterp(MetricObject):
+
+    distance_criteria = ["values", "grids"]
+
+    def __init__(self, values, grids, degree):
+
+        assert sklearn_available, "scikit-learn is not installed."
+
+        self.values = np.asarray(values)
+        self.grids = np.asarray(grids)
+        self.degree = degree
+
+        self.ndim = self.values.ndim
+        self.shape = self.values.shape
+
+        self.models = self._set_models()
+
+        self.X_train = np.c_[tuple(grid.ravel() for grid in self.grids)]
+
+        y_train = np.mgrid[[slice(0, dim) for dim in self.shape]]
+
+        self.y_train = np.c_[[y.ravel() for y in y_train]]
+
+        for dim in range(self.ndim):
+            self.models[dim].fit(self.X_train, self.y_train[dim])
+
+    def _set_models(self):
+
+        models = [
+            make_pipeline(
+                StandardScaler(),
+                PolynomialFeatures(degree=self.degree),
+                RidgeCV(),
+            )
+            for _ in range(self.ndim)
+        ]
+
+        return models
+
+    def __call__(self, *args):
+
+        args = np.asarray(args)
+
+        X_test = np.c_[tuple(arg.ravel() for arg in args)]
+
+        coordinates = np.empty_like(args)
+
+        for dim in range(self.ndim):
+            coordinates[dim] = self.models[dim].predict(X_test).reshape(args[0].shape)
+
+        output = map_coordinates(
+            self.values,
+            coordinates.reshape(args.shape[0], -1),
+        ).reshape(args[0].shape)
+
+        return output
+
+
+class RegularizedSplineInterp(RegularizedPolynomialInterp):
+    def __init__(self, values, grids, n_knots, degree):
+
+        self.n_knots = n_knots
+
+        super().__init__(values, grids, degree)
+
+    def _set_models(self):
+
+        models = [
+            make_pipeline(
+                StandardScaler(),
+                SplineTransformer(n_knots=self.n_knots, degree=self.degree),
+                RidgeCV(),
+            )
+            for _ in range(self.ndim)
+        ]
+
+        return models
+
