aea97f1a43f91db210a6cb49cfe9b2b3d1618f8e
Alan Lujan <alanlujan91@gmail.com>
2022-10-25 20:48:24 -0400

    move UtilityFuncCRRA

--- HARK/interpolation.py
+++ HARK/interpolation.py
@@ -13,17 +13,7 @@ import numpy as np
 from scipy.interpolate import CubicHermiteSpline
 
 from HARK.core import MetricObject
-from HARK.utilities import (
-    CRRAutility,
-    CRRAutility_inv,
-    CRRAutility_invP,
-    CRRAutilityP,
-    CRRAutilityP_inv,
-    CRRAutilityP_invP,
-    CRRAutilityPP,
-    CRRAutilityPPP,
-    CRRAutilityPPPP,
-)
+from HARK.utilities import CRRAutility, CRRAutilityP, CRRAutilityPP
 
 
 def _isscalar(x):
@@ -4536,131 +4526,6 @@ def calc_log_sum(Vals, sigma):
 ###############################################################################
 
 
-class UtilityFuncCRRA(MetricObject):
-    """
-    A class for representing a CRRA utility function.
-
-    Parameters
-    ----------
-    CRRA : float
-        The coefficient of constant relative risk aversion.
-    """
-
-    distance_criteria = ["CRRA"]
-
-    def __init__(self, CRRA):
-        self.CRRA = CRRA
-
-    def __call__(self, c, order=0):
-        """
-        Evaluate the utility function at a given level of consumption c.
-
-        Parameters
-        ----------
-        c : float or np.ndarray
-            Consumption level(s).
-        order : int, optional
-            Order of derivative. For example, `order == 1` returns the
-            first derivative of utility of consumption, and so on. By default 0.
-
-        Returns
-        -------
-        float or np.ndarray
-            Utility (or its derivative) evaluated at given consumption level(s).
-        """
-        if order == 0:
-            return CRRAutility(c, self.CRRA)
-        else:  # order >= 1
-            return self.derivative(c, order)
-
-    def derivative(self, c, order=1):
-        """
-        The derivative of the utility function at a given level of consumption c.
-
-        Parameters
-        ----------
-        c : float or np.ndarray
-            Consumption level(s).
-        order : int, optional
-            Order of derivative. For example, `order == 1` returns the
-            first derivative of utility of consumption, and so on. By default 1.
-
-        Returns
-        -------
-        float or np.ndarray
-            Derivative of CRRA utility evaluated at given consumption level(s).
-
-        Raises
-        ------
-        ValueError
-            Derivative of order higher than 4 is not supported.
-        """
-        if order == 1:
-            return CRRAutilityP(c, self.CRRA)
-        elif order == 2:
-            return CRRAutilityPP(c, self.CRRA)
-        elif order == 3:
-            return CRRAutilityPPP(c, self.CRRA)
-        elif order == 4:
-            return CRRAutilityPPPP(c, self.CRRA)
-        else:
-            raise ValueError("Derivative of order {} not supported".format(order))
-
-    def inverse(self, u, order=(0, 0)):
-        """
-        The inverse of the utility function at a given level of utility u.
-
-        Parameters
-        ----------
-        u : float or np.ndarray
-            Utility level(s).
-        order : tuple, optional
-            Order of derivatives. For example, `order == (1,1)` represents
-            the first derivative of utility, inversed, and then differenciated
-            once. For a simple mnemonic, order refers to the number of `P`s in
-            the function `CRRAutility[#1]_inv[#2]`. By default (0, 0),
-            which is just the inverse of utility.
-
-        Returns
-        -------
-        float or np.ndarray
-            Inverse of CRRA utility evaluated at given utility level(s).
-
-        Raises
-        ------
-        ValueError
-            Higher order derivatives are not supported.
-        """
-        if order == (0, 0):
-            return CRRAutility_inv(u, self.CRRA)
-        elif order == (1, 0):
-            return CRRAutilityP_inv(u, self.CRRA)
-        elif order == (0, 1):
-            return CRRAutility_invP(u, self.CRRA)
-        elif order == (1, 1):
-            return CRRAutilityP_invP(u, self.CRRA)
-        else:
-            raise ValueError("Inverse of order {} not supported".format(order))
-
-    def der(self, c, order=1):
-        """
-        Short alias for derivative. See `self.derivative`.
-        """
-        return self.derivative(c, order)
-
-    def inv(self, c, order=(0, 0)):
-        """
-        Short alias for inverse. See `self.inverse`.
-        """
-        return self.inverse(c, order)
-
-    def derinv(self, u, order=(1, 0)):
-        """
-        Short alias for inverse. See `self.inverse`.
-        """
-        return self.inverse(u, order)
-
-
 class ValueFuncCRRA(MetricObject):
     """
     A class for representing a value function.  The underlying interpolation is
@@ -4681,7 +4546,7 @@ class ValueFuncCRRA(MetricObject):
         self.vFuncNvrs = deepcopy(vFuncNvrs)
         self.CRRA = CRRA
 
-        if hasattr(vFuncNvrs, 'grid_list'):
+        if hasattr(vFuncNvrs, "grid_list"):
             self.grid_list = vFuncNvrs.grid_list
         else:
             self.grid_list = None
@@ -4704,7 +4569,7 @@ class ValueFuncCRRA(MetricObject):
         """
         #        return CRRAutility(self.func(*vFuncArgs), gam=self.CRRA)
         return CRRAutility(self.vFuncNvrs(*vFuncArgs), self.CRRA)
-    
+
     def gradient(self, *args):
 
         NvrsGrad = self.vFuncNvrs.gradient(*args)
@@ -4740,11 +4605,10 @@ class MargValueFuncCRRA(MetricObject):
         self.cFunc = deepcopy(cFunc)
         self.CRRA = CRRA
 
-        if hasattr(cFunc, 'grid_list'):
+        if hasattr(cFunc, "grid_list"):
             self.grid_list = cFunc.grid_list
         else:
             self.grid_list = None
-
 
     def __call__(self, *cFuncArgs):
         """
