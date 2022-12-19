3adb030e4103116f6d44bde069d26c28f0672293
AMonninger <66739352+AMonninger@users.noreply.github.com>
2022-12-19 05:03:11 -0500

    Journey 3

--- examples/ConsIndShockModel/IndShockConsumerType.py
+++ examples/ConsIndShockModel/IndShockConsumerType.py
@@ -8,9 +8,9 @@
 #       extension: .py
 #       format_name: percent
 #       format_version: '1.3'
-#       jupytext_version: 1.14.1
+#       jupytext_version: 1.13.7
 #   kernelspec:
-#     display_name: Python 3 (ipykernel)
+#     display_name: Python 3
 #     language: python
 #     name: python3
 # ---
