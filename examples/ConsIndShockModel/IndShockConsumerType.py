1a9b4f22e3199d046ecad0e4aef0bfcfa0f9dc12
gitkrakenAMonninger <amonnin1@jhu.edu>
2023-01-12 15:51:45 -0500

    IndShockConsumerType untouched

--- examples/ConsIndShockModel/IndShockConsumerType.py
+++ examples/ConsIndShockModel/IndShockConsumerType.py
@@ -8,9 +8,9 @@
 #       extension: .py
 #       format_name: percent
 #       format_version: '1.3'
-#       jupytext_version: 1.13.7
+#       jupytext_version: 1.14.1
 #   kernelspec:
-#     display_name: Python 3
+#     display_name: Python 3 (ipykernel)
 #     language: python
 #     name: python3
 # ---
