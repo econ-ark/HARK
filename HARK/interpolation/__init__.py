06bd770e081f29871dd86de646b93b767ada6808
Alan Lujan <alanlujan91@gmail.com>
2023-01-26 22:16:59 -0500

    add KernelRidge and GaussianProcessRegressor

--- HARK/interpolation/__init__.py
+++ HARK/interpolation/__init__.py
@@ -2,3 +2,4 @@ from HARK.interpolation.harkinterp import *
 from HARK.interpolation.econforgeinterp import *
 from HARK.interpolation.regularinterp import *
 from HARK.interpolation.unstructuredinterp import *
+from HARK.interpolation.sklearninterp import *
--- HARK/interpolation/sklearninterp.py
+++ HARK/interpolation/sklearninterp.py
@@ -1,36 +1,34 @@
 import numpy as np
 from scipy.ndimage import map_coordinates
+from sklearn.linear_model import RidgeCV
+from sklearn.kernel_ridge import KernelRidge
+from sklearn.gaussian_process import GaussianProcessRegressor
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import (
+    Normalizer,
+    PolynomialFeatures,
+    SplineTransformer,
+    StandardScaler,
+)
 
 from HARK.core import MetricObject
-
-
-try:
-    from sklearn.linear_model import RidgeCV
-    from sklearn.pipeline import make_pipeline
-    from sklearn.preprocessing import (
-        PolynomialFeatures,
-        SplineTransformer,
-        StandardScaler,
-    )
-
-    sklearn_available = True
-except ImportError:
-    sklearn_available = False
 
 DIM_MESSAGE = "Dimension mismatch."
 
 
-class RegularizedPolynomialInterp(MetricObject):
+class _SKLearnRegressionInterp(MetricObject):
 
+    standardize = True
+    normalize = True
     distance_criteria = ["values", "grids"]
 
-    def __init__(self, values, grids, degree, normalize=True):
-
-        assert sklearn_available, "scikit-learn is not installed."
+    def __init__(self, values, grids, standardize=False, normalize=False):
 
         self.values = np.asarray(values)
         self.grids = np.asarray(grids)
-        self.degree = degree
+
+        self.standardize = standardize
+        self.normalize = normalize
 
         self.ndim = self.values.ndim
         self.shape = self.values.shape
@@ -42,23 +40,22 @@ class RegularizedPolynomialInterp(MetricObject):
         y_train = np.mgrid[[slice(0, dim) for dim in self.shape]]
         self.y_train = np.c_[[y.ravel() for y in y_train]]
 
-        self.models = self._set_models(normalize=normalize)
+        pipeline = self._set_models()
+
+        self.models = [make_pipeline(*pipeline) for _ in range(self.ndim)]
         for dim in range(self.ndim):
             self.models[dim].fit(self.X_train, self.y_train[dim])
 
-    def _set_models(self, normalize=True):
+    def _set_models(self):
 
-        if normalize:
-            pipeline = [StandardScaler()]
-        else:
-            pipeline = []
+        pipeline = []
 
-        pipeline += [
-            PolynomialFeatures(degree=self.degree),
-            RidgeCV(alphas=np.logspace(-6, 6, 13)),
-        ]
+        if self.standardize:
+            pipeline += [StandardScaler()]
+        if self.normalize:
+            pipeline += [Normalizer()]
 
-        return [make_pipeline(pipeline) for _ in range(self.ndim)]
+        return pipeline
 
     def __call__(self, *args):
 
@@ -91,32 +88,102 @@ class RegularizedPolynomialInterp(MetricObject):
 
         output = map_coordinates(
             self.values,
-            coordinates.reshape(args.shape[0], -1),
-        ).reshape(args[0].shape)
+            coordinates.reshape(coordinates.shape[0], -1),
+        ).reshape(coordinates[0].shape)
 
         return output
 
 
-class RegularizedSplineInterp(RegularizedPolynomialInterp):
-    def __init__(self, values, grids, n_knots, degree, normalize=True):
+class _SKLearnFeaturesInterp(_SKLearnRegressionInterp):
+    def __init__(
+        self,
+        values,
+        grids,
+        degree=3,
+        n_knots=5,
+        polynomial=False,
+        spline=False,
+        standardize=False,
+        normalize=False,
+    ):
 
+        self.polynomial = polynomial
+        self.spline = spline
+
+        self.degree = degree
         self.n_knots = n_knots
 
-        super().__init__(values, grids, degree)
+        super().__init__(values, grids, standardize=standardize, normalize=normalize)
 
-    def _set_models(self, normalize=True):
+    def _set_models(self):
 
-        if normalize:
-            pipeline = [StandardScaler()]
-        else:
-            pipeline = []
+        pipeline = super()._set_models()
+
+        if self.polynomial:
+            pipeline += [PolynomialFeatures(degree=self.degree)]
+        elif self.spline:
+            pipeline += [SplineTransformer(n_knots=self.n_knots, degree=self.degree)]
+
+        return pipeline
+
+
+class RegularizedPolynomialInterp(_SKLearnRegressionInterp):
+    def __init__(self, values, grids, degree, standardize=False, normalize=False):
+
+        self.degree = degree
+
+        super().__init__(values, grids, standardize=standardize, normalize=normalize)
+
+    def _set_models(self):
+
+        pipeline = super()._set_models()
+
+        pipeline += [
+            PolynomialFeatures(degree=self.degree),
+            KernelRidge(),
+        ]
+
+        return pipeline
+
+
+class RegularizedSplineInterp(_SKLearnRegressionInterp):
+    def __init__(
+        self, values, grids, n_knots, degree, standardize=False, normalize=False
+    ):
+        self.degree = degree
+        self.n_knots = n_knots
+
+        super().__init__(values, grids, standardize=standardize, normalize=normalize)
+
+    def _set_models(self):
+
+        pipeline = super()._set_models()
 
         pipeline += [
             SplineTransformer(n_knots=self.n_knots, degree=self.degree),
-            RidgeCV(alphas=np.logspace(-6, 6, 13)),
+            KernelRidge(),
         ]
 
-        return [make_pipeline(pipeline) for _ in range(self.ndim)]
+        return pipeline
+
+
+class KernelRidgeInterp(_SKLearnRegressionInterp):
+    def _set_models(self):
+
+        pipeline = super()._set_models()
+
+        pipeline += [KernelRidge()]
+
+        return pipeline
+
+
+class GaussianProcessInterp(_SKLearnRegressionInterp):
+    def _set_models(self):
+        pipeline = super()._set_models()
+
+        pipeline += [GaussianProcessRegressor()]
+
+        return pipeline
 
 
 class UnstructuredPolynomialInterp(MetricObject):
