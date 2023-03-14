906ba4529bdc468b95bcfb1c7d5941f62de3c683
Alan Lujan <alanlujan91@gmail.com>
2023-03-14 17:44:16 -0400

    update WarpedInterpOnInterp2D

--- HARK/interpolation/_multi.py
+++ HARK/interpolation/_multi.py
@@ -28,7 +28,8 @@ class _RegularGridInterp(MetricObject):
     distance_criteria = ["values"]
 
     def __init__(self, values, target="cpu", **kwargs):
-        assert target in AVAILABLE_TARGETS, "Invalid target."
+        if target not in AVAILABLE_TARGETS:
+            raise ValueError("Invalid target.")
         self.target = target
 
         self.mc_kwargs = MC_KWARGS.copy()
@@ -49,9 +50,8 @@ class _RegularGridInterp(MetricObject):
         elif self.target == "gpu":
             args = cp.asarray(args)
 
-        assert (
-            args.shape[0] == self.ndim
-        ), "Number of arguments must match number of dimensions."
+        if args.shape[0] != self.ndim:
+            raise ValueError("Number of arguments must match number of dimensions.")
 
         coordinates = self._get_coordinates(args)
         return self._map_coordinates(coordinates)
@@ -75,23 +75,23 @@ class _RegularGridInterp(MetricObject):
 
 
 class MultivariateInterp(_RegularGridInterp):
+    distance_criteria = ["values", "grids"]
+
     def __init__(self, values, grids, target="cpu", **kwargs):
         super().__init__(values, target=target, **kwargs)
 
         if target == "cpu":
             self.grids = [np.asarray(grid) for grid in grids]
         elif target == "parallel":
-            self.grids = typed.List()
-            [self.grids.append(grid) for grid in grids]
+            self.grids = typed.List(grids)
         elif target == "gpu":
             self.grids = [cp.asarray(grid) for grid in grids]
 
-        assert self.ndim == len(
-            self.grids
-        ), "Number of grids must match number of dimensions."
-        assert all(
-            self.shape[i] == grid.size for i, grid in enumerate(self.grids)
-        ), "Values shape must match points in each grid."
+        if not (self.ndim == len(self.grids)):
+            raise ValueError("Number of grids must match number of dimensions.")
+
+        if not all(self.shape[i] == grid.size for i, grid in enumerate(self.grids)):
+            raise ValueError("Values shape must match points in each grid.")
 
     def _get_coordinates(self, args):
         if self.target == "cpu":
@@ -122,6 +122,8 @@ def _nb_interp(grids, args):
 
 
 class _CurvilinearGridInterp(_RegularGridInterp):
+    distance_criteria = ["values", "grids"]
+
     def __init__(self, values, grids, target="cpu"):
         super().__init__(values, target=target)
 
@@ -139,90 +141,132 @@ class _CurvilinearGridInterp(_RegularGridInterp):
 
 
 class WarpedInterpOnInterp2D(_CurvilinearGridInterp):
-    def __call__(self, *args):
+    def warmup(self):
+        self(*self.grids)
+
+        return None
+
+    def __call__(self, *args, axis=1):
         if self.target in ["cpu", "parallel"]:
             args = np.asarray(args)
         elif self.target == "gpu":
             args = cp.asarray(args)
 
-        assert args.shape[0] == self.ndim, "Wrong number of arguments."
+        if args.shape[0] != self.ndim:
+            raise ValueError("Number of arguments must match number of dimensions.")
 
         if self.target == "cpu":
-            output = self._target_cpu(args)
+            output = self._target_cpu(args, axis)
         elif self.target == "parallel":
-            output = self._target_parallel(args)
+            output = self._target_parallel(args, axis)
         elif self.target == "gpu":
-            output = self._target_gpu(args)
+            output = self._target_gpu(args, axis)
 
         return output
 
-    def _target_cpu(self, args):
+    def _target_cpu(self, args, axis):
         shape = args[0].shape  # original shape of arguments
+        size = args[0].size  # number of points in arguments
+        shape_axis = self.shape[axis]  # number of points in axis
 
         # flatten arguments by dimension
         args = args.reshape((self.ndim, -1))
 
-        # interpolate each dimension separately
-        # create intermediate arrays to store results
-        # the shape of each array is
-        #    - the number of points in the first dimension of the values
-        #    - the number of points in each dimension of the arguments
-        # so, (dimensions, points in argument)
-        y_intermed = np.empty((self.shape[0], args[0].size))
-        z_intermed = np.empty((self.shape[0], args[0].size))
+        y_intermed = np.empty((shape_axis, size))
+        z_intermed = np.empty((shape_axis, size))
 
-        for i in range(self.shape[1]):
+        for i in range(shape_axis):
             # for each dimension, interpolate the first argument
-            y_intermed[i] = np.interp(args[0], self.grids[0][:, i], self.grids[1][:, i])
-            z_intermed[i] = np.interp(args[0], self.grids[0][:, i], self.values[:, i])
+            grids0 = np.take(self.grids[0], i, axis=axis)
+            grids1 = np.take(self.grids[1], i, axis=axis)
+            values = np.take(self.values, i, axis=axis)
+            y_intermed[i] = np.interp(args[0], grids0, grids1)
+            z_intermed[i] = np.interp(args[0], grids0, values)
 
         output = np.empty_like(args[0])
 
-        for j in range(args[0].size):
-            output[j] = np.interp(args[1][j], y_intermed[:, j], z_intermed[:, j])
+        for j in range(size):
+            y_temp = y_intermed[:, j]
+            z_temp = z_intermed[:, j]
+
+            if y_temp[0] > y_temp[-1]:
+                # reverse
+                y_temp = y_temp[::-1]
+                z_temp = z_temp[::-1]
+
+            output[j] = np.interp(args[1][j], y_temp, z_temp)
 
         return output.reshape(shape)
 
-    def _target_parallel(self, args):
-        return nb_interp_piecewise(args, self.grids, self.values)
+    def _target_parallel(self, args, axis):
+        return nb_interp_piecewise(args, self.grids, self.values, axis)
 
-    def _target_gpu(self, args):
-        shape = args[0].shape
+    def _target_gpu(self, args, axis):
+        shape = args[0].shape  # original shape of arguments
+        size = args[0].size  # number of points in arguments
+        shape_axis = self.shape[axis]  # number of points in axis
 
         args = args.reshape((self.ndim, -1))
 
-        y_intermed = cp.empty((self.shape[0], args[0].size))
-        z_intermed = cp.empty((self.shape[0], args[0].size))
+        y_intermed = cp.empty((shape_axis, size))
+        z_intermed = cp.empty((shape_axis, size))
 
-        for i in range(self.shape[0]):
-            y_intermed[i] = cp.interp(args[0], self.grids[0][:, i], self.grids[1][:, i])
-            z_intermed[i] = cp.interp(args[0], self.grids[0][:, i], self.values[:, i])
+        for i in range(shape_axis):
+            # for each dimension, interpolate the first argument
+            grids0 = cp.take(self.grids[0], i, axis=axis)
+            grids1 = cp.take(self.grids[1], i, axis=axis)
+            values = cp.take(self.values, i, axis=axis)
+            y_intermed[i] = cp.interp(args[0], grids0, grids1)
+            z_intermed[i] = cp.interp(args[0], grids0, values)
 
         output = cp.empty_like(args[0])
 
-        for j in range(args[0].size):
-            output[j] = cp.interp(args[1][j], y_intermed[:, j], z_intermed[:, j])
+        for j in range(size):
+            y_temp = y_intermed[:, j]
+            z_temp = z_intermed[:, j]
+
+            if y_temp[0] > y_temp[-1]:
+                # reverse
+                y_temp = y_temp[::-1]
+                z_temp = z_temp[::-1]
+
+            output[j] = cp.interp(args[1][j], y_temp, z_temp)
 
         return output.reshape(shape)
 
 
 @njit(parallel=True, cache=True, fastmath=True)
-def nb_interp_piecewise(args, grids, values):
-    shape = args[0].shape
+def nb_interp_piecewise(args, grids, values, axis):
+    shape = args[0].shape  # original shape of arguments
+    size = args[0].size  # number of points in arguments
+    shape_axis = values.shape[axis]  # number of points in axis
 
+    # flatten arguments by dimension
     args = args.reshape((values.ndim, -1))
 
-    y_intermed = np.empty((values.shape[0], args[0].size))
-    z_intermed = np.empty((values.shape[0], args[0].size))
+    y_intermed = np.empty((shape_axis, size))
+    z_intermed = np.empty((shape_axis, size))
 
-    for i in prange(values.shape[0]):
-        y_intermed[i] = np.interp(args[0], grids[0][:, i], grids[1][:, i])
-        z_intermed[i] = np.interp(args[0], grids[0][:, i], values[:, i])
+    for i in prange(shape_axis):
+        # for each dimension, interpolate the first argument
+        grids0 = grids[0][i] if axis == 0 else grids[0][:, i]
+        grids1 = grids[1][i] if axis == 0 else grids[1][:, i]
+        vals = values[i] if axis == 0 else values[:, i]
+        y_intermed[i] = np.interp(args[0], grids0, grids1)
+        z_intermed[i] = np.interp(args[0], grids0, vals)
 
     output = np.empty_like(args[0])
 
-    for j in prange(args[0].size):
-        output[j] = np.interp(args[1][j], y_intermed[:, j], z_intermed[:, j])
+    for j in prange(size):
+        y_temp = y_intermed[:, j]
+        z_temp = z_intermed[:, j]
+
+        if y_temp[0] > y_temp[-1]:
+            # reverse
+            y_temp = y_temp[::-1]
+            z_temp = z_temp[::-1]
+
+        output[j] = np.interp(args[1][j], y_temp, z_temp)
 
     return output.reshape(shape)
 
