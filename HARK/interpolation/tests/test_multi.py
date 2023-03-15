dbd7564f519750763c7df536d945816607006b12
Alan Lujan <alanlujan91@gmail.com>
2023-03-14 20:07:21 -0400

    update tests

---
+++ HARK/interpolation/tests/test_multi.py
@@ -0,0 +1,45 @@
+import unittest
+from math import isclose
+from typing import List
+
+import numpy as np
+
+from HARK.interpolation import MultivariateInterp
+
+
+class TestMultivariateInterp(unittest.TestCase):
+    def setUp(self):
+        # create test data
+
+        self.grids = [
+            np.linspace(-1, 1, 4),
+            np.linspace(-2, 2, 5),
+            np.linspace(-3, 3, 6),
+        ]
+
+        grids = np.meshgrid(*self.grids, indexing="ij")
+
+        self.values = grids[0] + grids[1] + grids[2]
+
+        self.interpolator_cpu = MultivariateInterp(
+            self.values, self.grids, target="cpu"
+        )
+        self.interpolator_parallel = MultivariateInterp(
+            self.values, self.grids, target="parallel"
+        )
+
+    def test_interpolation_values(self):
+        # check that interpolation values match expected values
+        args = [0.1, 1.5, 2.9]
+        expected_value = np.sum(args)
+
+        value_cpu = self.interpolator_cpu(*args)
+        value_parallel = self.interpolator_parallel(*args)
+
+        self.assertTrue(isclose(value_cpu, expected_value, rel_tol=1e-9))
+        self.assertTrue(isclose(value_parallel, expected_value, rel_tol=1e-9))
+
+    def test_invalid_target_error(self):
+        # check that specifying an invalid target raises an error
+        with self.assertRaises(ValueError):
+            _ = MultivariateInterp(self.values, self.grids, target="invalid")
