5cbf55adbf067cf05355c164c0584ec3e8b942d1
Adam Turner <9087854+aa-turner@users.noreply.github.com>
2023-05-23 00:44:26 +0100

    Organise configuration values by section

--- Documentation/conf.py
+++ Documentation/conf.py
@@ -1,6 +1,10 @@
-# Synchronise with Sphinx requirement in 'requirements/dev.txt'
-needs_sphinx = "6.1"
+# Project information
+project = "HARK"
+copyright = "2020, Econ-ARK team"
+author = "Econ-ARK team"
+version = release = "latest"
 
+# General configuration
 extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.autosummary",
@@ -16,51 +20,50 @@ extensions = [
     "recommonmark",
 ]
 
-# This is currently not working
-nbsphinx_execute = "never"
-
-# Extend theme width
-html_css_files = ["theme_overrides.css"]
-
-autodoc_default_flags = ["members"]  # must add outside ']' bracket
-autosummary_generate = True
-
-# Add any paths that contain templates here, relative to this directory.
-templates_path = ["_templates"]
-
-source_suffix = [
-    ".rst",
-    ".md",
-]
-
-# The master toctree document.
-master_doc = "index"
-
-# General information about the project.
-project = "HARK"
-copyright = "2020, Econ-ARK team"
-author = "Econ-ARK team"
-
-version = release = "latest"
-
-language = "en"
-
 exclude_patterns = [
     "_build",
     "Thumbs.db",
     ".DS_Store",
 ]
 
+language = "en"
+
+master_doc = "index"
+
+# Synchronise with Sphinx requirement in 'requirements/dev.txt'
+needs_sphinx = "6.1"
+
 pygments_style = "sphinx"
 
-# Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {
-    "python": ("https://docs.python.org/3/", None),
-}
+source_suffix = [
+    ".rst",
+    ".md",
+]
 
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ["_templates"]
+
+# HTML writer configuration
 html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+
+# Extend theme width
+html_css_files = ["theme_overrides.css"]
+
+# sphinx.ext.intersphinx configuration
+intersphinx_mapping = {
+    "python": ("https://docs.python.org/3/", None),
+}
+
+# sphinx.ext.autodoc configuration
+autodoc_default_flags = ["members"]  # must add outside ']' bracket
+
+# sphinx.ext.autosummary configuration
+autosummary_generate = True
+
+# nbsphinx configuration
+nbsphinx_execute = "never"  # This is currently not working
