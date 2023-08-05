09b0f5318c40dad03ceb5653d524d3e747243b44
Adam Turner <9087854+AA-Turner@users.noreply.github.com>
2023-08-05 13:03:46 +0100

    Fix ``nb_exec.py`` (#1332)

--- tools/nb_exec.py
+++ tools/nb_exec.py
@@ -10,34 +10,43 @@ Notebooks are executed in parallel, with one worker
 per processor in the host machine.
 """
 
-from pathlib import Path
+import multiprocessing
+import os
 import sys
-from concurrent.futures import ProcessPoolExecutor
+import time
+from pathlib import Path
 
 import nbformat
 from nbclient import NotebookClient
 
-ROOT_DIR = Path(__file__).parent.parent
+ROOT_DIR = Path(__file__).resolve().parent.parent
 
 
-def run_notebook(notebook_file):
+def run_notebook(notebook_file: Path):
     rel_file_name = notebook_file.relative_to(ROOT_DIR).as_posix()
     print(f'{rel_file_name}: Loading notebook')
-    nb = nbformat.read(notebook_file, as_version=4)
-    client = NotebookClient(nb, timeout=600, kernel_name='python3', record_timing=False)
-    print(f'{rel_file_name}: Executing')
-    client.execute()
-    print(f'{rel_file_name}: Writing')
-    nbformat.write(nb, notebook_file)
-    print(f'{rel_file_name}: Finished')
-    del nb, client
+    try:
+        # Journey-PhD and LifecycleModel expect execution from their own directory
+        os.chdir(notebook_file.parent)
+        nb = nbformat.read(notebook_file, as_version=4)
+        client = NotebookClient(nb, timeout=600, kernel_name='python3', record_timing=False)
+        print(f'{rel_file_name}: Executing')
+        start = time.perf_counter()
+        client.execute()
+        elapsed = time.perf_counter() - start
+        print(f'{rel_file_name}: Writing')
+        nbformat.write(nb, notebook_file)
+        print(f'{rel_file_name}: Finished (executed in {elapsed:.2f}s)')
+        del nb, client, start, elapsed
+    except Exception as err:
+        print(f'{rel_file_name}: Failed to execute\n   {err}', file=sys.stderr)
 
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
-        notebooks = sys.argv[1:]
+        notebooks = (Path(p).resolve() for p in sys.argv[1:])
     else:
         notebooks = ROOT_DIR.joinpath('examples').rglob('*.ipynb')
 
-    with ProcessPoolExecutor() as pool:
+    with multiprocessing.Pool() as pool:
         pool.map(run_notebook, notebooks)
