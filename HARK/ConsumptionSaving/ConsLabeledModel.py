ee0e663ebf99c89f4e33de191c494071b9e61525
Matthew N. White <mnwhite@gmail.com>
2023-09-25 11:26:14 -0400

    Adjustments so that labeled model and tests work
    ConsLabeledModel inherits from IndShockConsumerType, but doesn't use cFunc as an attribute of solution, so add_stable_points can't be run for it; skip post_solve. Added missing T_cycle length in one example notebook, which was generating an error with check_conditions().

--- HARK/ConsumptionSaving/ConsLabeledModel.py
+++ HARK/ConsumptionSaving/ConsLabeledModel.py
@@ -303,6 +303,9 @@ class PerfForesightLabeledType(IndShockConsumerType):
             continuation=None,
             attrs={"m_nrm_min": 0.0},  # minimum normalized market resources
         )
+        
+    def post_solve(self):
+        pass # Do nothing, rather than try to run calc_stable_points
 
 
 class ConsPerfForesightLabeledSolver(ConsIndShockSetup):
