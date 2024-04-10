ccd45f66c9670c29db6d35fd85b81fc39215aeb1
Matthew N. White <mnwhite@gmail.com>
2024-04-10 14:47:20 -0400

    Re-add seemingly redundant update_solution_terminal
    This one call *looks* redundant, but something goes wildly wrong with the Jacobian methods test without it. Test passes locally with this line re-added.

--- HARK/ConsumptionSaving/ConsIndShockModel.py
+++ HARK/ConsumptionSaving/ConsIndShockModel.py
@@ -2661,6 +2661,7 @@ class IndShockConsumerType(PerfForesightConsumerType):
         # Update all income process variables to match any attributes that might
         # have been changed since `__init__` or `solve()` was last called.
         #        self.update_income_process()
+        self.update_solution_terminal()
         if not self.quiet:
             self.check_conditions(verbose=self.verbose)
 
