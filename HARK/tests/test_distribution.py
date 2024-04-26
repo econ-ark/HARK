ff134b633e646bafb40dcc5d734c51db9679751c
Matthew N. White <mnwhite@gmail.com>
2024-04-26 17:13:42 -0400

    Fix indexing error in distribution tests
    I think something about pytest just changed while I was working on this branch, because three tests needed to have a [0] added to them (to reference the only element in the array) in order to be correct. Nothing changed, but the result of these tests did. I'm confused.

--- HARK/tests/test_distribution.py
+++ HARK/tests/test_distribution.py
@@ -44,7 +44,7 @@ class DiscreteDistributionTests(unittest.TestCase):
         norm = Normal(mu=-(sig**2) / 2, sigma=sig).discretize(131, method="hermite")
         my_logn = distr_of_function(norm, func=lambda x: np.exp(x))
         exp = calc_expectation(my_logn)
-        self.assertAlmostEqual(exp, 1.0)
+        self.assertAlmostEqual(exp[0], 1.0)
 
         # Function 1 -> n
         # Mean and variance of the normal
@@ -184,7 +184,7 @@ class DiscreteDistributionTests(unittest.TestCase):
         norm = Normal(mu=-(sig**2) / 2, sigma=sig).discretize(131, method="hermite")
         my_logn = norm.dist_of_func(lambda x: np.exp(x))
         exp = my_logn.expected()
-        self.assertAlmostEqual(exp, 1.0)
+        self.assertAlmostEqual(exp[0], 1.0)
 
         # Function 1 -> n
         # Mean and variance of the normal
@@ -336,7 +336,7 @@ class DistributionClassTests(unittest.TestCase):
         self.assertEqual(uni_discrete.atoms[0][0], 0.0)
         self.assertEqual(uni_discrete.atoms[0][-1], 1.0)
         self.assertEqual(
-            calc_expectation(uni.discretize(10, method="equiprobable")), 0.5
+            calc_expectation(uni.discretize(10, method="equiprobable"))[0], 0.5
         )
 
     def test_Bernoulli(self):
