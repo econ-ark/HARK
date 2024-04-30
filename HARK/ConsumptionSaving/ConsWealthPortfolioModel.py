e036cf856ce028f2495ef8763b74322b3ff21897
alanlujan91 <alanlujan91@gmail.com>
2024-04-30 14:12:55 -0400

    update solver

--- HARK/ConsumptionSaving/ConsWealthPortfolioModel.py
+++ HARK/ConsumptionSaving/ConsWealthPortfolioModel.py
@@ -16,12 +16,93 @@ from HARK.interpolation import (
 )
 from HARK.rewards import UtilityFuncCRRA
 from HARK.utilities import NullFunc
-from scipy.optimize import root_scalar
+
+
+import numpy as np
+from HARK.interpolation import LinearInterp
+
+
+class ChiFromOmegaFunction:
+    """
+    A class for representing a function that takes in values of omega = EndOfPrdvP / aNrm
+    and returns the corresponding optimal chi = cNrm / aNrm. The only parameters
+    that matter for this transformation are the coefficient of relative risk
+    aversion rho and the share of wealth in the Cobb-Douglas aggregator delta.
+
+    Parameters
+    ----------
+    rho : float
+        Coefficient of relative risk aversion.
+    delta : float
+        Share for wealth in the Cobb-Douglas aggregator in CRRA utility function.
+    N : int, optional
+        Number of interpolating gridpoints to use (default 501).
+    z_bound : float, optional
+        Absolute value on the auxiliary variable z's boundary (default 15).
+        z represents values that are input into a logit transformation
+        scaled by the upper bound of chi, which yields chi values.
+    """
+
+    def __init__(self, CRRA, WealthShare, N=501, z_bound=15):
+        self.CRRA = CRRA
+        self.WealthShare = WealthShare
+        self.N = N
+        self.z_bound = z_bound
+
+        self.update()
+
+    def f(self, x):
+        """
+        Define the relationship between chi and omega, and evaluate on the vector
+        """
+        return (
+            (1 - self.WealthShare) * x ** (-self.WealthShare)
+            - self.WealthShare * x ** (1 - self.WealthShare)
+        ) ** (-1 / self.CRRA) * x ** (1 - self.WealthShare)
+
+    def update(self):
+        """
+        Construct the underlying interpolation of log(omega) on z.
+        """
+        # Make vectors of chi and z
+        chi_limit = (1.0 - self.WealthShare) / self.WealthShare
+        z_vec = np.linspace(-self.z_bound, self.z_bound, self.N)
+        exp_z = np.exp(z_vec)
+        chi_vec = chi_limit * exp_z / (1 + exp_z)
+
+        omega_vec = self.f(chi_vec)
+        log_omega_vec = np.log(omega_vec)
+
+        # Construct the interpolant
+        zFromLogOmegaFunc = LinearInterp(log_omega_vec, z_vec, lower_extrap=True)
+
+        # Store the function and limit as attributes
+        self.func = zFromLogOmegaFunc
+        self.limit = chi_limit
+
+    def __call__(self, omega):
+        """
+        Calculate optimal values of chi = cNrm / aNrm from values of omega.
+
+        Parameters
+        ----------
+        omega : np.array
+            One or more values of omega = EndOfPrdvP / aNrm.
+
+        Returns
+        -------
+        chi : np.array
+            Identically shaped array with optimal chi values.
+        """
+        z = self.func(np.log(omega))
+        exp_z = np.exp(z)
+        chi = self.limit * exp_z / (1 + exp_z)
+        return np.nan_to_num(chi)
 
 
 class WealthPortfolioConsumerType(PortfolioConsumerType):
     time_inv_ = deepcopy(PortfolioConsumerType.time_inv_)
-    time_inv_ = time_inv_ + ["WealthShare", "WealthShift"]
+    time_inv_ = time_inv_ + ["WealthShare", "WealthShift", "ChiFunc"]
 
     def __init__(self, **kwds):
         params = init_wealth_portfolio.copy()
@@ -32,6 +113,11 @@ class WealthPortfolioConsumerType(PortfolioConsumerType):
         super().__init__(**kwds)
 
         self.solve_one_period = solve_one_period_WealthPortfolio
+
+        if self.WealthShare == 0.0:
+            self.ChiFunc = None
+        else:
+            self.ChiFunc = ChiFromOmegaFunction(self.CRRA, self.WealthShare)
 
 
 def utility(c, a, CRRA, share=0.0, intercept=0.0):
@@ -85,6 +171,16 @@ def du2_diff(c, a=None, CRRA=None, share=None, intercept=None, vp_a=None):
 def du2_jac(c, a, CRRA, share, intercept, vp_a):
     du2_diag = du2_diff(c, a, CRRA, share, intercept, vp_a)
     return np.diag(du2_diag)
+
+
+def chi_ratio(c, a, intercept):
+    return c / (a + intercept)
+
+
+def chi_func(chi, CRRA, share):
+    return chi ** (1 - share) * (
+        (1 - share) * chi ** (-share) - share * chi ** (1 - share)
+    ) ** (-1 / CRRA)
 
 
 def euler(c, a, CRRA, share, intercept, vp_a):
@@ -197,6 +293,7 @@ def solve_one_period_WealthPortfolio(
     vFuncBool,
     WealthShare,
     WealthShift,
+    ChiFunc,
 ):
     # Make sure the individual is liquidity constrained.  Allowing a consumer to
     # borrow *and* invest in an asset with unbounded (negative) returns is a bad mix.
@@ -330,29 +427,10 @@ def solve_one_period_WealthPortfolio(
     # Now this is where we look for optimal C
     # for each a in the agrid find corresponding c that satisfies the euler equation
 
-    cNrm_now = np.empty_like(aNrmGrid)
-
-    for a_idx, a_nrm in enumerate(aNrmGrid):
-        cNrm_guess = euler(
-            aNrmGrid,
-            a_nrm,
-            CRRA,
-            WealthShare,
-            WealthShift,
-            end_dvda_now[a_idx],
-        )
-
-        lower_bound = aNrmGrid[cNrm_guess > 0][-1]
-        upper_bound = aNrmGrid[cNrm_guess < 0][0]
-
-        result = root_scalar(
-            euler,
-            method="toms748",
-            bracket=[lower_bound, upper_bound],
-            args=(a_nrm, CRRA, WealthShare, WealthShift, end_dvda_now[a_idx]),
-        )
-
-        cNrm_now[a_idx] = result.root
+    if WealthShare == 0.0:
+        cNrm_now = end_dvda_nvrs_now
+    else:
+        cNrm_now = ChiFunc(end_dvda_nvrs_now) * (aNrmGrid + WealthShift)
 
     # Calculate the endogenous mNrm gridpoints when the agent adjusts his portfolio,
     # then construct the consumption function when the agent can adjust his share
