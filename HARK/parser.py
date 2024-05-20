ba35d5af740044e143fa26e10ed57a46ebbf0d28
sb <sbenthall@gmail.com>
2024-05-20 16:43:06 -0400

    adding the parser module

---
+++ HARK/parser.py
@@ -0,0 +1,28 @@
+from sympy.utilities.lambdify import lambdify
+from  sympy.parsing.sympy_parser import parse_expr
+
+class Expression():
+
+    def __init__(self, text):
+        self.txt
+        self.expr = parse_expr(text)
+        self.npf = self.func()
+
+        # first derivatives.
+        self.grad = {
+            sym.__str__() : 
+            self.expr.diff(sym)
+            for sym
+            in list(self.expr.free_symbols)
+        }
+
+    def func(self):
+        return lambdify(list(self.expr.free_symbols), self.expr, "numpy")
+
+def math_text_to_lambda(text):
+    """
+    Returns a function represented by the given mathematical text.
+    """
+    expr = parse_expr(text)
+    func = lambdify(list(expr.free_symbols), expr, "numpy")
+    return func
