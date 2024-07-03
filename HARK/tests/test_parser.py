2db65276463175478d68cdda2e69151788ad76d3
sb <sbenthall@gmail.com>
2024-07-03 09:47:55 -0400

    passing tests for parsing block configuration and building working DBlock, normalized consumption

--- HARK/tests/test_parser.py
+++ HARK/tests/test_parser.py
@@ -1,7 +1,7 @@
 import os
 import unittest
 
-
+import HARK.model as model
 import HARK.parser as parser
 import yaml
 
@@ -17,35 +17,12 @@ class test_consumption_parsing(unittest.TestCase):
         self.consumer_yaml_file
 
         config = yaml.load(self.consumer_yaml_file, Loader=parser.harklang_loader())
-        pass
 
-        """
-        try:
-            config = yaml.load(open('perfect_foresight_full_experimental.yaml', 'r'), Loader= get_loader())
+        self.assertEqual(config['calibration']['DiscFac'], 0.96)
+        self.assertEqual(config['blocks'][0]['name'], 'consumption normalized')
 
-            # data is copied
-            assert config['model']['blocks']['consumption'] == config['model']['strategies'][1]['block']
-            # data is maintained once in memory and referenced in both places
-            assert config['model']['blocks']['consumption'] is config['model']['strategies'][1]['block']
-
-            # object created by parser
-            c1 = config['model']['blocks']['consumption']['dynamics']['c']
-            c2 = config['model']['strategies'][1]['block']['dynamics']['c']
-
-            # objects are equal
-            assert c1 == c2
-            # objects are identical in memory; the reference is shared.
-                assert c1 is c2
-
-            a_str = config['model']['blocks']['consumption']['dynamics']['a']
-            a_expr = parse_expr(a_str)
-            a_func = lambdify(list(a_expr.free_symbols), a_expr, "numpy")
-
-            m = np.random.random(100)
-            c = np.random.random(100)
-
-            #import pdb; pdb.set_trace()
-
-        except yaml.YAMLError as exc:
-            print("Error in configuration file:", exc)
-        """
+        ## construct and test the block
+        cons_norm_block = model.DBlock(**config['blocks'][0])
+        cons_norm_block.construct_shocks(config['calibration'])
+        cons_norm_block.discretize({"theta": {"N": 5}})
+        self.assertEqual(cons_norm_block.calc_reward({"c": 1, "CRRA": 2})["u"], -1.0)
