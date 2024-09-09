a5d229cae9f0019780774829dcbf3656f0c5c28b
Matthew N. White <mnwhite@gmail.com>
2024-09-09 15:35:12 -0400

    Modernization fixes for calc_euler_error_func
    It looks like this method lacks tests and was never updated for changes in HARK.distribution. This commit fixes some notation so that the method works again.

--- HARK/ConsumptionSaving/ConsIndShockModel.py
+++ HARK/ConsumptionSaving/ConsIndShockModel.py
@@ -2303,7 +2303,7 @@ class IndShockConsumerType(PerfForesightConsumerType):
                 tail_bound=[0.05, 0.95],
             )
             TranShkDstn = add_discrete_outcome_constant_mean(
-                TranShkDstn, self.UnempPrb, self.IncUnemp
+                TranShkDstn, p=self.UnempPrb, x=self.IncUnemp
             )
             PermShkDstn = MeanOneLogNormal(sigma=self.PermShkStd[0]).discretize(
                 N=200,
@@ -2331,12 +2331,12 @@ class IndShockConsumerType(PerfForesightConsumerType):
         aNowGrid = mNowGrid - cNowGrid
 
         # Tile the grids for fast computation
-        ShkCount = IncShkDstn[0].size
+        ShkCount = IncShkDstn.pmv.size
         aCount = aNowGrid.size
         aNowGrid_tiled = np.tile(aNowGrid, (ShkCount, 1))
-        PermShkVals_tiled = (np.tile(IncShkDstn[1], (aCount, 1))).transpose()
-        TranShkVals_tiled = (np.tile(IncShkDstn[2], (aCount, 1))).transpose()
-        ShkPrbs_tiled = (np.tile(IncShkDstn[0], (aCount, 1))).transpose()
+        PermShkVals_tiled = (np.tile(IncShkDstn.atoms[0], (aCount, 1))).transpose()
+        TranShkVals_tiled = (np.tile(IncShkDstn.atoms[1], (aCount, 1))).transpose()
+        ShkPrbs_tiled = (np.tile(IncShkDstn.pmv, (aCount, 1))).transpose()
 
         # Calculate marginal value next period for each gridpoint and each shock
         mNextArray = (
