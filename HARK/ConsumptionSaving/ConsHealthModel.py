b935ba0fb5c3c58bfd29798f6dfee7a0dc9fc60d
Matthew N. White <mnwhite@gmail.com>
2025-05-22 17:34:15 -0400

    Begin work on BasicHealthConsumerType
    Committing because I ran out of time for the day.

--- HARK/ConsumptionSaving/ConsHealthModel.py
+++ HARK/ConsumptionSaving/ConsHealthModel.py
@@ -4,16 +4,22 @@ first model here is adapted from White (2015).
 """
 
 import numpy as np
-from HARK.distributions import expected
+from HARK.core import AgentType
+from HARK.distributions import (
+    expected,
+    combine_indep_dstns,
+    DiscreteDistributionLabeled,
+)
 from HARK.rewards import CRRAutility, CRRAutility_inv
 from HARK.interpolation import Curvilinear2DMultiInterp
+from HARK.utilities import make_assets_grid
 
 ###############################################################################
 
 
 # Define a function to transition from end-of-period states to succeeding states
 def get_states_next(shock, a, H, R):
-    m_next = R * a + shock["WageRte"] * H
+    m_next = R * a + shock["IncShk"] * H
     h_next = (1.0 - shock["DeprRte"]) * H
     return m_next, h_next
 
@@ -26,14 +32,14 @@ def eval_health_prod(n, alpha, gamma):
 # Define a function for computing expectations over next period's (marginal) value
 # from the perspective of end-of-period states, conditional on survival
 def calc_exp_next(shock, a, H, R, rho, alpha, gamma, funcs):
-    m_next = R * a + shock["WageRte"] * H
+    m_next = R * a + shock["IncShk"] * H
     h_next = (1.0 - shock["DeprRte"]) * H
     vNvrs_next, c_next, n_next = funcs(m_next, h_next)
     dvdm_next = c_next**-rho
     dvdh_next = dvdm_next / (gamma * n_next ** (alpha - 1.0))
     v_next = CRRAutility(vNvrs_next, rho=rho)
     dvda = R * dvdm_next
-    dvdH = (1.0 - shock["DeprRte"]) * (shock["WageRte"] * dvdm_next + dvdh_next)
+    dvdH = (1.0 - shock["DeprRte"]) * (shock["IncShk"] * dvdm_next + dvdh_next)
     return v_next, dvda, dvdH
 
 
@@ -68,7 +74,7 @@ def solve_one_period_ConsBasicHealth(
         Risk-free rate of return on retained assets.
     CRRA : float
         Coefficient of relative risk aversion, representing rho. Assumed to be
-        constant across periods.
+        constant across periods. Should be strictly between 0 and 1.
     HealthProdExp : float
         Exponent in health production function; should be strictly b/w 0 and 1.
         This corresponds to alpha in White (2015).
@@ -78,7 +84,7 @@ def solve_one_period_ConsBasicHealth(
     DieProbMax : float
         Maximum death probability at the end of this period, if HLvl were exactly zero.
     ShockDstn : DiscreteDistribution
-        Joint distribution of wage and depreciation values that could realize
+        Joint distribution of income and depreciation values that could realize
         at the start of the next period.
     aLvlGrid : np.array
         Grid of end-of-period assets (after all actions are accomplished).
@@ -134,3 +140,124 @@ def solve_one_period_ConsBasicHealth(
     # Construct solution as a multi-interpolation
     solution_now = Curvilinear2DMultiInterp([vNvrs, cLvl, nLvl], mLvl, hLvl)
     return solution_now
+
+
+def make_solution_terminal_ConsBasicHealth():
+    """
+    Constructor for the terminal period solution for the basic health investment
+    model. The trivial solution is to consume all market resources and invest
+    nothing in health. Takes no parameters because CRRA is irrelevant: pseudo-inverse
+    value is returned rather than value, and the former is just cLvl = mLvl.
+
+    The solution representation for this model is a multiple output function that
+    takes market resources and health capital level as inputs and returns pseudo-
+    inverse value, consumption level, and health investment level in that order.
+    """
+    return lambda mLvl, hLvl: (mLvl, mLvl, np.zeros_like(mLvl))
+
+
+def make_health_grid(hLvlMin, hLvlMax, hLvlCount):
+    """
+    Make a uniform grid of health capital levels.
+
+    Parameters
+    ----------
+    hLvlMin : float
+        Lower bound on health capital level; should almost surely be zero.
+    hLvlMax : float
+        Upper bound on health capital level.
+    hLvlCount : int
+        Number of points in uniform health capital level grid.
+
+    Returns
+    -------
+    hLvlGrid : np.array
+        Uniform grid of health capital levels
+    """
+    return np.linspace(hLvlMin, hLvlMax, hLvlCount)
+
+
+def combine_indep_inc_and_depr_dstns(T_cycle, IncShkDstn, DeprRteDstn, RNG):
+    """
+    Combine univariate distributions of income realizations and depreciation rate
+    realizations at each age, treating them as independent.
+
+    Parameters
+    ----------
+    T_cycle : int
+        Number of periods in the agent's sequence of periods (cycle).
+    IncShkDstn : [DiscreteDistribution]
+        Age-dependent list of income realizations; should have length T_cycle.
+    DeprRteDstn : [DiscreteDistribution]
+        Age-dependent list of health depreciation rate realizatiosn; should have
+        length T_cycle.
+    RNG : np.random.RandomState
+        Internal random number generator for the AgentType instance.
+
+    Returns
+    -------
+    ShockDstn : [DiscreteDistribution]
+        Age-dependent bivariate distribution with joint realizations of income
+        and health depreciation rates.
+    """
+    if len(IncShkDstn) != T_cycle:
+        raise ValueError(
+            "IncShkDstn must be a list of distributions of length T_cycle!"
+        )
+    if len(DeprRteDstn) != T_cycle:
+        raise ValueError(
+            "DeprRteDstn must be a list of distributions of length T_cycle!"
+        )
+    ShockDstnX = [
+        combine_indep_dstns(
+            IncShkDstn[t], DeprRteDstn[t], seed=RNG.integers(0, 2**31 - 1)
+        )
+        for t in range(T_cycle)
+    ]
+    ShockDstn = DiscreteDistributionLabeled.from_unlabeled(
+        dstn=ShockDstnX,
+        name="income and depreciation shock distribution",
+        var_names=["IncShk", "DeprRte"],
+    )
+    return ShockDstn
+
+
+###############################################################################
+
+basic_health_constructors = {
+    "solution_terminal": make_solution_terminal_ConsBasicHealth,
+    "IncShkDstn": make_lognormal_income_dstn,
+    "DeprRteDstn": make_uniform_depreciation_dstn,
+    "ShockDstn": combine_indep_inc_and_depr_dstns,
+    "aLvlGrid": make_assets_grid,
+    "hLvlGrid": make_health_grid,
+    "DieProbMax": make_logistic_polynomial_die_prob,
+}
+
+basic_health_simple_params = {
+    "constructors": basic_health_constructors,
+    "DiscFac": 0.96,
+    "Rfree": [1.03],
+    "CRRA": 0.5,
+    "HealthProdExp": 0.35,
+    "HealthProdFac": 1.0,
+    "T_cycle": 1,
+}
+
+init_basic_health = {}
+init_basic_health.update(basic_health_simple_params)
+
+
+class BasicHealthConsumerType(AgentType):
+    """
+    A class to represent consumers who can save in a risk-free asset and invest
+    in the health capital via a health production function. The model is a slight
+    alteration of the one from White (2015), which was in turn lifted from Ludwig
+    and Schoen. In this variation, survival probability depends on post-investment
+    health capital, rather than next period's health capital realization.
+    """
+
+    defaults_ = {
+        "params": init_basic_health,
+        "solver": solve_one_period_ConsBasicHealth,
+    }
