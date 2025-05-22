2c597a833a2bfe48d7c5296a229b7f90f6d10598
Matthew N. White <mnwhite@gmail.com>
2025-05-22 15:15:26 -0400

    Ugh pre-commit

--- HARK/interpolation.py
+++ HARK/interpolation.py
@@ -4092,7 +4092,7 @@ class Curvilinear2DInterp(HARKinterpolator2D):
             raise ValueError("x_values must have the same shape as f_values!")
         if not (my_shape == y_values.shape):
             raise ValueError("y_values must have the same shape as f_values!")
-        
+
         self.f_values = f_values
         self.x_values = x_values
         self.y_values = y_values
@@ -4419,14 +4419,14 @@ class Curvilinear2DInterp(HARKinterpolator2D):
         # Calculate the derivative with respect to x (and return it)
         dfdy = y_alpha * dfda + y_beta * dfdb
         return dfdy
-    
+
 
 class Curvilinear2DMultiInterp(Curvilinear2DInterp):
     """
     An extension of Curvilinear2DInterp that allows multiple function outputs.
     All of the interpolated functions must share a common curvilinear grid.
     """
-    
+
     def __init__(self, f_values, x_values, y_values):
         N_funcs = len(f_values)
         my_shape = x_values.shape
@@ -4434,8 +4434,10 @@ class Curvilinear2DMultiInterp(Curvilinear2DInterp):
             raise ValueError("y_values must have the same shape as x_values!")
         for n in range(N_funcs):
             if not (my_shape == f_values[0].shape):
-                raise ValueError("Each element of f_values must have the same shape as x_values!")
-        
+                raise ValueError(
+                    "Each element of f_values must have the same shape as x_values!"
+                )
+
         self.f_values = f_values
         self.x_values = x_values
         self.y_values = y_values
@@ -4443,6 +4445,7 @@ class Curvilinear2DMultiInterp(Curvilinear2DInterp):
         self.y_n = my_shape[1]
         self.N_funcs = N_funcs
         self.update_polarity()
+
 
 class DiscreteInterp(MetricObject):
     """
