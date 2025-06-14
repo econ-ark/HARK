f100c4e2493fd9a57138582900a794ff37a88f17
Matthew N. White <mnwhite@gmail.com>
2025-06-13 20:53:38 -0400

    Further notebook fix
    I didn't actually use the dictionary before.

--- HARK/ConsumptionSaving/ConsNewKeynesianModel.py
+++ HARK/ConsumptionSaving/ConsNewKeynesianModel.py
@@ -185,10 +185,8 @@ class NewKeynesianConsumerType(IndShockConsumerType):
         else:
             m_points = num_pointsM
 
-        if not isinstance(timestonest, int):
+        if timestonest is None:
             timestonest = self.mFac
-        else:
-            timestonest = timestonest
 
         if self.cycles == 0:
             if not hasattr(dist_mGrid, "__len__"):
